348116e9f8881a681607a28fbd3c9bc345790c4c
Arthur Debert <arthur@stimuli.com.br>
2011-11-24 12:20:03 -0200

    Ignoring warnings on dev runserver for yummier ouput

--- dev-runserver.sh
+++ dev-runserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if test "x$1" = "xplus"; then
-    python manage.py runserver_plus 0.0.0.0:8000 --settings=dev_settings
+    python -Wi manage.py runserver_plus 0.0.0.0:8000 --settings=dev_settings
 else
-    python manage.py runserver 0.0.0.0:8000 --settings=dev_settings
+    python -Wi manage.py runserver 0.0.0.0:8000 --settings=dev_settings
 fi
