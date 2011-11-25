f3765cacf36bb1925e995c70d9be64fc00fbaf66
Arthur Debert <arthur@stimuli.com.br>
2011-11-25 13:23:04 -0200

    Finished Safe Email Backend, added domain matching plus safeguard not to activate on production

--- utils/safemail.py
+++ utils/safemail.py
@@ -2,7 +2,10 @@
 Loops through all recipients (to, bcc, cc) and removes all non white listed
 addresses before sending through smtp.
 Requires the setting:
-EMAIL_NOTIFICATION_RECEIVERS = (sequence of emails )
+EMAIL_NOTIFICATION_RECEIVERS = (sequence of emails or email terminations)
+Example:
+EMAIL_NOTIFICATION_RECEIVERS = ("arthur@example.com", "@pculture.org")
+Will only send email to the first anddress and any emails ending with @pculture.org
 This way, only unisubs crew will ever receive notifications.
 """
 
@@ -19,7 +22,16 @@ class InternalOnlyBackend(object):
         self.white_listed_addresses = getattr(settings, "EMAIL_NOTIFICATION_RECEIVERS", [])
 
     def get_whitelisted(self, addresses):
-        return [x for x in addresses if x in self.white_listed_addresses]
+        clean = []
+        for x in addresses:
+            if x in self.white_listed_addresses:
+                clean.append(x)
+            else:
+                for white_listed in self.white_listed_addresses:
+                    if x.endswith(white_listed):
+                        clean.append(x)
+                        break
+        return clean
         
     def send_messages(self, email_messages):
         self.file_backend.send_messages(email_messages)
@@ -28,6 +40,3 @@ class InternalOnlyBackend(object):
             message.bcc = self.get_whitelisted(message.bcc)
             message.cc = self.get_whitelisted(message.cc)
         self.smtp_backend.send_messages(email_messages)
-            
-            
-            
