c01642206aa9238e3e637dda1c70392d9be36254
Steve Losh <steve@stevelosh.com>
2011-12-15 12:55:08 -0500

    Add an experimental member search AJAX view.

--- apps/teams/views.py
+++ apps/teams/views.py
@@ -740,6 +740,16 @@ def highlight(request, slug, highlight=True):
     return redirect(request.META.get('HTTP_REFERER', '/'))
 
 
+@render_to_json
+def search_members(request, slug):
+    team = Team.get(slug, request.user)
+    q = request.GET.get('q')
+
+    results = [[m.user.id, m.user.username]
+               for m in team.members.filter(user__username__icontains=q)]
+
+    return { 'results': results }
+
 # Tasks
 TEAM_LANGUAGES = []
 
