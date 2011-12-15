51953eef6c5d355321b5d568d20cd51c1c78f21f
Nick Sergeant <nick@nicksergeant.com>
2011-12-15 10:57:39 -0500

    Um, yeah, a much better way to do this.

--- get-last-commit-url.py
+++ get-last-commit-url.py
@@ -5,25 +5,17 @@
 # PyQuery: http://pypi.python.org/pypi/pyquery
 # Requests: http://docs.python-requests.org/en/latest/index.html
 
-import re, requests, sys
-from pyquery import PyQuery as pq
+import subprocess, sys
 
 
 def main():
-    r = requests.get('https://github.com/pculture/unisubs/commits/ongoing')
-    J = pq(r.content)
 
-    commits = J('p.commit-title')
-    coms = []
+    output = subprocess.check_output('git log --no-merges -1 --pretty=oneline', shell=True)
 
-    for commit in commits:
-        if not re.match('.*merge.*', J(commit).text(), flags=re.IGNORECASE):
-            coms.append(commit)
+    hash, title = output.split(' ', 1)
 
-    title = '%s\n\n' % J('a', coms[0]).eq(0).text()
-    sys.stderr.write(title)
-
-    sys.stdout.write('https://github.com%s'.rstrip() % J('a', coms[0]).attr('href'))
+    sys.stderr.write('%s\n' % title)
+    sys.stdout.write('https://github.com/pculture/unisubs/commit/%s' % hash)
 
 if __name__ == '__main__':
     main()
