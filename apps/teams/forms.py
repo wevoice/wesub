ea786357920d6ea259c6da22d70d694292df41f4
Steve Losh <steve@stevelosh.com>
2011-12-28 10:56:58 -0500

    Fix the add video form's clean() method.
    For https://unisubs.sifterapp.com/projects/12298/issues/512831/comments

--- apps/teams/forms.py
+++ apps/teams/forms.py
@@ -216,11 +216,13 @@ class AddTeamVideoForm(BaseVideoBoundForm):
     def clean(self):
         language = self.cleaned_data['language']
         video = self.fields['video_url'].video
-        original_sl = video.subtitle_language()
 
-        if video and (original_sl and not original_sl.language) and not language:
-            msg = _(u'Set original language for this video.')
-            self._errors['language'] = self.error_class([msg])
+        if video:
+            original_sl = video.subtitle_language()
+
+            if (original_sl and not original_sl.language) and not language:
+                msg = _(u'Set original language for this video.')
+                self._errors['language'] = self.error_class([msg])
 
         return self.cleaned_data
 
