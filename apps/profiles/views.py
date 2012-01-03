b2f2a73df604ce00a86fee717c97430b18128c00
Arthur Debert <arthur@stimuli.com.br>
2012-01-03 13:59:09 -0200

    Showing latest 10 items for action feed in user dashboard

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -82,7 +82,7 @@ def dashboard(request):
     user = request.user
     context = {
         'user_info': user,
-        'action_list': Action.objects.for_user(user)[:5]
+        'action_list': Action.objects.for_user(user)[:10]
     }
 
     return direct_to_template(request, 'profiles/dashboard.html', context)
