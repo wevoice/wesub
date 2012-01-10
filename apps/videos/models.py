d3ec29928a46853f6319353b9f08840cb054b28d
Steve Losh <steve@stevelosh.com>
2012-01-10 16:58:28 -0500

    Fix video URLs containing a # sign.

--- apps/videos/models.py
+++ apps/videos/models.py
@@ -281,7 +281,10 @@ class Video(models.Model):
         NOTE: this method is used in videos.search_indexes.VideoSearchResult
         to prevent duplication of code in search result and in DB-query result
         """
-        return self.title.replace('/', '-').replace('?', '-').replace('&', '-')
+        return (self.title.replace('/', '-')
+                          .replace('?', '-')
+                          .replace('#', '-')
+                          .replace('&', '-'))
 
     def _get_absolute_url(self, locale=None):
         """
