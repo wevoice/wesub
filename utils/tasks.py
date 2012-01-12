3ffea173c206d4c8a58953eceb34d80fb66d609b
Arthur Debert <arthur@stimuli.com.br>
2012-01-12 11:59:34 -0200

    Allow check_user_preferences for email to be passed from async templated email send

--- utils/tasks.py
+++ utils/tasks.py
@@ -3,6 +3,6 @@ from utils import send_templated_email
 
 @task
 def send_templated_email_async(to, subject, body_template, body_dict, 
-                         from_email=None, ct="html", fail_silently=False):
+                         from_email=None, ct="html", fail_silently=False, check_user_preference=True):
     return send_templated_email(to,subject, body_template, body_dict, 
                          from_email=None, ct="html", fail_silently=False)
