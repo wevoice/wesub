dd76233ef83684e159881c11974b4f75289e49e4
Arthur Debert <arthur@stimuli.com.br>
2012-01-12 13:07:07 -0200

    Made sure we propagate the check user preference to async template send

--- utils/tasks.py
+++ utils/tasks.py
@@ -2,7 +2,9 @@ from celery.task import task
 from utils import send_templated_email
 
 @task
-def send_templated_email_async(to, subject, body_template, body_dict, 
-                         from_email=None, ct="html", fail_silently=False, check_user_preference=True):
-    return send_templated_email(to,subject, body_template, body_dict, 
-                         from_email=None, ct="html", fail_silently=False)
+def send_templated_email_async(
+    to, subject, body_template, body_dict, from_email=None, ct="html",
+    fail_silently=False, check_user_preference=True):
+    return send_templated_email(
+        to,subject, body_template, body_dict, from_email=None, ct="html",
+        fail_silently=False, check_user_preference=check_user_preference)
