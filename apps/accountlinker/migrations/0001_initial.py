0bd73be5e10c8169362dc04f4137f162f9d7528b
Arthur Debert <arthur@stimuli.com.br>
2012-01-27 17:57:58 -0200

    Dough, index length was on max_length for username, fixes migration and life in general

--- apps/accountlinker/migrations/0001_initial.py
+++ apps/accountlinker/migrations/0001_initial.py
@@ -4,16 +4,13 @@ from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
-# mysql is barfing on the longer index name
-
-key_name = 'accountlinker_tpa'
 class Migration(SchemaMigration):
     
     def forwards(self, orm):
         
         # Adding model 'ThirdPartyAccount'
         db.create_table('accountlinker_thirdpartyaccount', (
-            ('username', self.gf('django.db.models.fields.CharField')(max_length=512, db_index=True)),
+            ('username', self.gf('django.db.models.fields.CharField')(max_length=256, db_index=True)),
             ('oauth_refresh_token', self.gf('django.db.models.fields.CharField')(max_length=256, db_index=True)),
             ('type', self.gf('django.db.models.fields.CharField')(max_length=10)),
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
@@ -22,7 +19,7 @@ class Migration(SchemaMigration):
         db.send_create_signal('accountlinker', ['ThirdPartyAccount'])
 
         # Adding unique constraint on 'ThirdPartyAccount', fields ['type', 'oauth_access_token']
-        db.create_unique(key_name, ['type', 'oauth_access_token'])
+        db.create_unique('accountlinker_thirdpartyaccount', ['type', 'oauth_access_token'])
     
     
     def backwards(self, orm):
@@ -31,7 +28,7 @@ class Migration(SchemaMigration):
         db.delete_table('accountlinker_thirdpartyaccount')
 
         # Removing unique constraint on 'ThirdPartyAccount', fields ['type', 'oauth_access_token']
-        db.delete_unique(key_name, ['type', 'oauth_access_token'])
+        db.delete_unique('accountlinker_thirdpartyaccount', ['type', 'oauth_access_token'])
     
     
     models = {
@@ -41,7 +38,7 @@ class Migration(SchemaMigration):
             'oauth_access_token': ('django.db.models.fields.CharField', [], {'max_length': '256', 'db_index': 'True'}),
             'oauth_refresh_token': ('django.db.models.fields.CharField', [], {'max_length': '256', 'db_index': 'True'}),
             'type': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
-            'username': ('django.db.models.fields.CharField', [], {'max_length': '512', 'db_index': 'True'})
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '256', 'db_index': 'True'})
         }
     }
     
--- apps/accountlinker/models.py
+++ apps/accountlinker/models.py
@@ -33,7 +33,7 @@ class ThirdPartyAccount(models.Model):
     """
     type = models.CharField(max_length=10, choices=ACCOUNT_TYPES)
     # this is the third party account user name, eg the youtube user
-    username  = models.CharField(max_length=512, db_index=True, 
+    username  = models.CharField(max_length=256, db_index=True, 
                                  null=False, blank=False)
     oauth_access_token = models.CharField(max_length=256, db_index=True, 
                                           null=False, blank=False)
