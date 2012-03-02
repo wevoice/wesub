9d008aea43a502c048af5cb1aa11fdfdc570b3c8
Steve Losh <steve@stevelosh.com>
2012-03-02 11:05:06 -0500

    Clean up whitespace, comments, typos, dead code, etc.

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,12 +1,9 @@
 from django.conf import settings
 
 def media(request):
-    """
-    Adds media-related context variables to the context.
-
-    """
+    """Add media-related context variables to the context."""
     return {
         'MEDIA_URL': settings.MEDIA_URL, 
         'STATIC_URL': settings.STATIC_URL, 
-        "STATIC_URL_BASE": settings.STATIC_URL_BASE,
-        "COMPRESS_MEDIA": settings.COMPRESS_MEDIA }
+        'STATIC_URL_BASE': settings.STATIC_URL_BASE,
+        'COMPRESS_MEDIA': settings.COMPRESS_MEDIA }
