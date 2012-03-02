710e2db91b194cd4f6132d74f22c148575548b49
Steve Losh <steve@stevelosh.com>
2012-03-02 11:37:58 -0500

    More cleanup.

--- utils/amazon/fields.py
+++ utils/amazon/fields.py
@@ -1,16 +1,18 @@
-from sorl.thumbnail.base import Thumbnail
-from sorl.thumbnail.main import build_thumbnail_name
-from StringIO import StringIO        
-from django.conf import settings
-from django.db import models
-from django.db.models.fields.files import FieldFile
-from boto.s3.connection import S3Connection
-from django.core.files.base import ContentFile
+from StringIO import StringIO
 from hashlib import sha1
+from thread import start_new_thread
 from time import time
 from uuid import uuid4
-from thread import start_new_thread
-from django.core.files.storage import FileSystemStorage
+
+from boto.s3.connection import S3Connection
+from django.conf import settings
+from django.core.files.base import ContentFile
+from django.db import models
+from django.db.models.fields.files import FieldFile
+from sorl.thumbnail.base import Thumbnail
+from sorl.thumbnail.main import build_thumbnail_name
+from south.modelsinspector import add_introspection_rules
+
 
 THUMB_SIZES = getattr(settings, 'THUMBNAILS_SIZE', ())
 USE_THREADED_THUMBNAIL_CREATING = getattr(settings, 'USE_THREADED_THUMBNAIL_CREATING', False)
@@ -28,23 +30,22 @@ def create_thumbnails(obj, content, size=None, thumb_name=None):
         obj.storage.save(th_name, ContentFile(img.read()))
 
 class S3ImageFieldFile(FieldFile):
-    
     def thumb_url(self, w, h):
         if not self.name:
             return ''
-        
+
         name = self.build_thumbnail_name(self.name, (w, h))
         if not settings.USE_AMAZON_S3 and not self.storage.exists(name) and self.storage.exists(self.name):
             create_thumbnails(self, self.storage.open(self.name), (w, h), name)
         return self.storage.url(name)
-    
+
     def generate_file_name(self):
         return sha1(settings.SECRET_KEY+str(time())+str(uuid4())).hexdigest()
-    
+
     def build_thumbnail_name(self, name, size, options=None):
         options = options or self.field.thumb_options
         return build_thumbnail_name(name, size, options)
-    
+
     def save(self, name, content, save=True):
         ext = name.split('.')[-1]
         name = '%s.%s' % (self.generate_file_name(), ext)
@@ -55,7 +56,7 @@ class S3ImageFieldFile(FieldFile):
         # Update the filesize cache
         self._size = len(content)
         self._committed = True
-        
+
         if USE_THREADED_THUMBNAIL_CREATING:
             start_new_thread(create_thumbnails, (self, content))
         else:
@@ -74,11 +75,11 @@ class S3ImageFieldFile(FieldFile):
             del self.file
 
         self.storage.delete(self.name)
-        
+
         for size in self.field.thumb_sizes:
             name = self.build_thumbnail_name(self.name, size)
             self.storage.delete(name)
-        
+
         self.name = None
         setattr(self.instance, self.field.name, self.name)
 
@@ -92,61 +93,58 @@ class S3ImageFieldFile(FieldFile):
     delete.alters_data = True
 
 class S3EnabledImageField(models.ImageField):
-
     attr_class = S3ImageFieldFile
-    
-    def __init__(self, bucket=settings.AWS_USER_DATA_BUCKET_NAME, 
-                       thumb_sizes=THUMB_SIZES, thumb_options=dict(crop='smart', upscale=True), 
+
+    def __init__(self, bucket=settings.AWS_USER_DATA_BUCKET_NAME,
+                       thumb_sizes=THUMB_SIZES, thumb_options=dict(crop='smart', upscale=True),
                        verbose_name=None, name=None, width_field=None, height_field=None, **kwargs):
         self.thumb_sizes = thumb_sizes
         self.thumb_options = thumb_options
         self.bucket_name = bucket
-        
+
         if settings.USE_AMAZON_S3:
             self.connection = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
-            
-            if self.bucket_name == settings.AWS_USER_DATA_BUCKET_NAME:
-                from utils.amazon import default_s3_store
-                storage = default_s3_store
-                self.bucket = storage.bucket
-            else:            
-                from utils.amazon import S3Storage
-                if not self.connection.lookup(bucket):
-                    self.connection.create_bucket(bucket)
-                self.bucket = self.connection.get_bucket(bucket)
-                storage = S3Storage(self.bucket)
-                
-            kwargs['storage'] = storage
-        super(S3EnabledImageField, self).__init__(verbose_name, name, width_field, height_field, **kwargs)
-    
-class S3EnabledFileField(models.FileField):
-    
-    def __init__(self, bucket=settings.AWS_USER_DATA_BUCKET_NAME, verbose_name=None, name=None, upload_to='', storage=None, **kwargs):
-        self.bucket_name = bucket
-        
-        if settings.USE_AMAZON_S3:
-            self.connection = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
-            
+
             if self.bucket_name == settings.AWS_USER_DATA_BUCKET_NAME:
                 from utils.amazon import default_s3_store
                 storage = default_s3_store
                 self.bucket = storage.bucket
             else:
                 from utils.amazon import S3Storage
-                
                 if not self.connection.lookup(bucket):
                     self.connection.create_bucket(bucket)
                 self.bucket = self.connection.get_bucket(bucket)
                 storage = S3Storage(self.bucket)
-        super(S3EnabledFileField, self).__init__(verbose_name, name, upload_to, storage, **kwargs)    
 
-from south.modelsinspector import add_introspection_rules
+            kwargs['storage'] = storage
+        super(S3EnabledImageField, self).__init__(verbose_name, name, width_field, height_field, **kwargs)
+
+class S3EnabledFileField(models.FileField):
+    def __init__(self, bucket=settings.AWS_USER_DATA_BUCKET_NAME, verbose_name=None, name=None, upload_to='', storage=None, **kwargs):
+        self.bucket_name = bucket
+
+        if settings.USE_AMAZON_S3:
+            self.connection = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+
+            if self.bucket_name == settings.AWS_USER_DATA_BUCKET_NAME:
+                from utils.amazon import default_s3_store
+                storage = default_s3_store
+                self.bucket = storage.bucket
+            else:
+                from utils.amazon import S3Storage
+
+                if not self.connection.lookup(bucket):
+                    self.connection.create_bucket(bucket)
+                self.bucket = self.connection.get_bucket(bucket)
+                storage = S3Storage(self.bucket)
+        super(S3EnabledFileField, self).__init__(verbose_name, name, upload_to, storage, **kwargs)
+
 
 add_introspection_rules([
     (
-        [S3EnabledImageField, S3EnabledFileField], 
+        [S3EnabledImageField, S3EnabledFileField],
         [],
-        {   
+        {
             "bucket": ["bucket_name", {"default": settings.AWS_USER_DATA_BUCKET_NAME}]
         },
     ),
@@ -154,9 +152,9 @@ add_introspection_rules([
 
 add_introspection_rules([
     (
-        [S3EnabledImageField], 
+        [S3EnabledImageField],
         [],
-        {   
+        {
             "thumb_sizes": ["thumb_sizes", {"default": THUMB_SIZES}],
             "thumb_options": ["thumb_options", {"default": dict(crop='smart')}]
         },
