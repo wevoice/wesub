c87280acf2a2505f0229d450c8dafa52e13ef1e6
Arthur Debert <arthur@stimuli.com.br>
2012-03-05 21:16:25 -0300

    Included video thumbnails on thumbnail fixing

--- apps/videos/management/commands/fix_thumbnails.py
+++ apps/videos/management/commands/fix_thumbnails.py
@@ -7,6 +7,7 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         from teams.models import Team
+        from videos.models import Video
         
         for team in Team.objects.exclude(logo=''):
             print team.logo, 
@@ -27,3 +28,13 @@ class Command(BaseCommand):
                 user.picture = ''
                 user.save()
                 print 'S3 ERROR'                
+
+        for video in Video.objects.exclude(s3_thumbnail=''):
+            print video.s3_thumbnail, 
+            try:
+                create_thumbnails(video.s3_thumbnail, video.s3_thumbnail.file)
+                print 'FIXED'
+            except S3ResponseError:
+                video.s3_thumbnail = ''
+                video.save()
+                print 'S3 ERROR'
