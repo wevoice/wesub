4088acc74b4554de0a62e8069598c071347bace7
Nick Sergeant <nick@nicksergeant.com>
2012-03-20 16:15:10 -0400

    Check if task.subtitle_version exists before altering on delete.

--- apps/teams/views.py
+++ apps/teams/views.py
@@ -1260,7 +1260,7 @@ def delete_task(request, slug):
 
             if task.get_type_display() in ['Review', 'Approve']:
                 # TODO: Handle subtitle/translate tasks here too?
-                if not form.cleaned_data['discard_subs']:
+                if not form.cleaned_data['discard_subs'] and task.subtitle_version:
                     task.subtitle_version.moderation_status = MODERATION.APPROVED
                     task.subtitle_version.save()
                     metadata_manager.update_metadata(video.pk)
