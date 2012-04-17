9e6ab309655f7981bc3e1d9d42faaf777404f912
Nick Sergeant <nick@nicksergeant.com>
2012-04-17 14:16:40 -0400

    Cherry pick 'Some UniSubs -> Amara stragglers.' (44633bf587bef82919fba38c2a36414b3dd54ade) to staging

--- utils/tasks.py
+++ utils/tasks.py
@@ -10,3 +10,15 @@ def send_templated_email_async(to, subject, body_template, body_dict,
     return send_templated_email(
         to,subject, body_template, body_dict, from_email=None, ct="html",
         fail_silently=False, check_user_preference=check_user_preference)
+
+
+@periodic_task(run_every=crontab(minute=0, hour=1))
+def send_error_report(date=None):
+    date = date or datetime.datetime.now() - datetime.timedelta(days=1)
+    recipients = getattr(settings,'SEND_ERROR_REPORT_TO', None)
+    if not recipients:
+        return 
+    data = errorreport._error_report_data(date)
+    message = render_to_string("internal/error-report.txt", data)
+    subject = date.strftime("Errors for Amara for %Y/%M/%d")
+    send_mail(subject, message, "unisubs error bot", recipients)
