69cdae62ed8a431610a78c162380857a398e6dd0
Fernando Takai <fernando.takai@gmail.com>
2012-04-26 18:05:09 -0300

    If the user has a task for a subtitle, allow him to edit drafts from the revision screen

--- apps/videos/views.py
+++ apps/videos/views.py
@@ -379,6 +379,8 @@ def legacy_history(request ,video, lang=None):
 
 @get_video_from_code
 def history(request, video, lang=None, lang_id=None, version_id=None):
+    from teams.models import Task
+
     if not lang:
         return HttpResponseRedirect(video.get_absolute_url(video_id=video._video_id_used))
     elif lang == 'unknown':
@@ -451,6 +453,15 @@ def history(request, video, lang=None, lang_id=None, version_id=None):
     context['rollback_allowed'] = version and not version.video.is_moderated
     context['last_version'] = version
     context['next_version'] = version.next_version() if version else None
+    context['can_edit'] = False
+
+    if request.user.is_authenticated():
+        # user can only edit a subtitle draft if he
+        # has a subtitle/translate task assigned to him
+        tasks = Task.objects.incomplete_subtitle_or_translate()\
+                            .filter(team_video=video.get_team_video(), assignee=request.user, language=language.language)
+
+        context['can_edit'] = tasks.exists()
 
     return render_to_response("videos/subtitle-view.html", context, context_instance=RequestContext(request))
 
