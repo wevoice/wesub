ca4c5152749c4af2b7b97170bc43b99b1d33b5d0
Rob Chekaluk <rchekaluk@pculture.org>
2012-05-02 07:40:05 -0400

    More typos.

--- deploy/update_translations.sh
+++ deploy/update_translations.sh
@@ -5,7 +5,7 @@ set -e
 
 date
 
-# Git pull from Transifex with retries
+# Git pull with retries
 successful_pull=""
 set +e
 
@@ -34,7 +34,7 @@ python manage.py makemessages -d djangojs -i deploy\* -i media\js\closure-librar
 echo "pushing to transifex"
 tx push --source
 
-echo "pulling from transilex"
+echo "pulling from transifex"
 tx pull
 
 echo "compiling messages"
@@ -44,7 +44,7 @@ echo "adding to git"
 git add locale/*/LC_MESSAGES/django.*o
 
 echo "committing to rep"
-git commit -m "Updated transiflex translations -- through update_translations.sh"
+git commit -m "Updated transifex translations -- through update_translations.sh"
 
 echo "pushing to rep"
 date
