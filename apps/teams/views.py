628172fd2bcd73e4cc91e9a330f024a15bf4999a
Craig Zheng <craig.zheng@gmail.com>
2012-05-02 12:58:29 -0400

    Fix tasks view when sorting by expiration date

--- apps/teams/views.py
+++ apps/teams/views.py
@@ -1091,15 +1091,9 @@ def _order_tasks(request, tasks):
     elif sort == '-created':
         tasks = tasks.order_by('-created')
     elif sort == 'expires':
-        # Unfortunately, MySQL puts NULL records to the top, here.
-        # In this sorting instance, we convert the two querysets to
-        # lists and combine them, forcing the NULL records to the bottom.
-        # Alternative would be to write custom SQL to drop the NULLs.
-        null_expirations = tasks.filter(expiration_date=None)
-        has_expirations = tasks.exclude(expiration_date=None)
-        tasks = list(has_expirations.order_by('expiration_date')) + list(null_expirations)
+        tasks = tasks.exclude(expiration_date=None).order_by('expiration_date')
     elif sort == '-expires':
-        tasks = tasks.order_by('-expiration_date')
+        tasks = tasks.exclude(expiration_date=None).order_by('-expiration_date')
 
     return tasks
 
