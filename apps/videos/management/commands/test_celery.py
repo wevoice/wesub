010165bf3c7f81c9f9a6381109c2ce040e57ef3d
Honza Pokorny <me@honza.ca>
2012-05-29 11:57:47 -0300

    Revert "Fix import error"
    This reverts commit 9e174ef1562ddfdc22c33c15e7f9a5013c40a99e.
    
    A lot more things depend on the `add` task than I thought.

--- apps/videos/management/commands/test_celery.py
+++ apps/videos/management/commands/test_celery.py
@@ -17,7 +17,7 @@
 # http://www.gnu.org/licenses/agpl-3.0.html.
 
 from django.core.management.base import BaseCommand
-from testhelpers.tasks import add
+from videos.tasks import add
 from optparse import make_option
 import time
 
