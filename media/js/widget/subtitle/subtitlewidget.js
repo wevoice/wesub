76e223160491b09b609060ef114989e1367728ab
Arthur Debert <arthur@stimuli.com.br>
2012-06-12 19:45:49 -0300

    Fixed new line to br convertion, fixes https://unisubs.sifterapp.com/projects/12298/issues/606511/comments

--- media/js/widget/subtitle/subtitlewidget.js
+++ media/js/widget/subtitle/subtitlewidget.js
@@ -280,7 +280,9 @@ unisubs.subtitle.SubtitleWidget.prototype.updateValues_ = function() {
         if (time != -1)
             this.timeSpinner_.setValue(time);
     }
-    this.titleElemInner_.innerHTML = this.subtitle_.getText().replace("\n", "<br/>");
+    console.log('new');
+    this.titleElemInner_.innerHTML = goog.string.newLineToBr(this.subtitle_.getText());
+
 };
 unisubs.subtitle.SubtitleWidget.prototype.disposeEventHandlers_ = function() {
     if (this.keyHandler_) {
