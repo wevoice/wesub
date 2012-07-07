7183a59c25a059ae2b37de4344e0d96f59a69535
Evan Hazlett <ejhazlett@gmail.com>
2012-07-07 13:11:26 -0400

    updated vagrant puppet setup

--- puppet/lucid64.pp
+++ puppet/lucid64.pp
@@ -4,13 +4,25 @@ class lucid64 {
     python  => true,
     nodejs  => false,
   }
-  include config
   include celery
+  include closure
   include nginx
   include rabbitmq
   include redis
   include solr
   include mysql
+  class { 'config':
+    require => [
+      Class['appserver'],
+      Class['celery'],
+      Class['closure'],
+      Class['nginx'],
+      Class['solr'],
+      Class['mysql'],
+      Class['rabbitmq'],
+      Class['redis'],
+    ],
+  }
 }
 
 class { "lucid64": }
