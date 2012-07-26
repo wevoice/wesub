76a0694604c65f2830dab088e3dad4f24f99b768
Steve Losh <steve@stevelosh.com>
2012-07-26 11:43:43 -0400

    Split the Subtitle wrapper objects out into their own file.

--- apps/subtitles/tests/objects.py
+++ apps/subtitles/tests/objects.py
@@ -3,25 +3,24 @@
 #
 # Copyright (C) 2012 Participatory Culture Foundation
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see
-# http://www.gnu.org/licenses/agpl-3.0.html.
+# You should have received a copy of the GNU Affero General Public License along
+# with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html.
 
 """Tests for the subtitle objects implementation."""
 
 from django.test import TestCase
 
-from apps.subtitles.models import Subtitle, SubtitleSet
+from apps.subtitles.objects import Subtitle, SubtitleSet
 
 
 class TestSubtitle(TestCase):
@@ -182,12 +181,12 @@ class TestSubtitleSet(TestCase):
         self.assertRaises(AssertionError, _set(raw))
         self.assertRaises(AssertionError, _set(None))
 
-        def _set_slice(v):
-            def _s():
-                ss[0:1] = v
-            return _s
-
         # TODO: Fix slice assignment.
+        # def _set_slice(v):
+        #     def _s():
+        #         ss[0:1] = v
+        #     return _s
+
         # _set_slice([good])
         # self.assertRaises(AssertionError, _set_slice([bare]))
         # self.assertRaises(AssertionError, _set_slice([raw]))
