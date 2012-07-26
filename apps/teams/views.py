aa436389d39f2ae1835be61a1b116a27d13c7530
Honza Pokorny <me@honza.ca>
2012-07-26 10:45:25 -0300

    Don't blow up on unicode

--- apps/teams/views.py
+++ apps/teams/views.py
@@ -1752,7 +1752,7 @@ def get_billing_data_for_team(team, start_date, end_date, header=True):
             end = subs[-1].end_time
 
             rows.append([
-                tv.video.title,
+                tv.video.title.encode('utf-8'),
                 tv.video.get_video_url(),
                 tv.video.language,
                 round(end - start)
