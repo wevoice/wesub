56229d7e3e88bb86b2274a5d232e9cc19982e39d
Steve Losh <steve@stevelosh.com>
2012-07-27 12:33:20 -0400

    Remove the unused livesettings app.

--- apps/videos/admin.py
+++ apps/videos/admin.py
@@ -24,12 +24,8 @@ from videos.models import (
 from videos.tasks import video_changed_tasks
 
 from django.core.urlresolvers import reverse
-from django.utils.translation import ugettext_lazy as _
-from utils.livesettings_values import EmailListValue
-from livesettings import BASE_GROUP, config_register
 from utils.celery_search_index import update_search_index
 
-config_register(EmailListValue(BASE_GROUP, 'alert_emails', description=_(u'Email for alert')))
 
 class VideoUrlInline(admin.StackedInline):
     model = VideoUrl
