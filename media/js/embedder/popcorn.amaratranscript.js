6057c7b390f012e65671845e28098d3bcbaf992a
Nick Sergeant <nick@nicksergeant.com>
2012-08-16 14:48:15 -0400

    Add new Embedder files.

---
+++ media/js/embedder/popcorn.amaratranscript.js
@@ -0,0 +1,38 @@
+(function (Popcorn) {
+    Popcorn.plugin( 'amaratranscript' , {
+        _setup : function(options) {
+
+            options.pop = this;
+            options.lineHtml = document.createElement('a');
+            options.lineHtml.href = '#';
+            options.lineHtml.classList.add('amara-group');
+            options.lineHtml.classList.add('amara-transcript-line');
+
+            var lineTemplate = '' +
+                '<span class="amara-transcript-line-left">' + options.start_clean + '</span>' +
+                '<span class="amara-transcript-line-right">' + options.text + '</span>';
+
+            options.lineHtml.innerHTML = lineTemplate;
+            options.container.appendChild(options.lineHtml);
+
+            options.lineHtml.addEventListener('click', function(e) {
+                options.pop.currentTime(options.start);
+                e.preventDefault();
+                e.stopPropagation();
+            }, false);
+
+        },
+        start: function(event, options){
+            options.lineHtml.classList.add('current-subtitle');
+        },
+        end: function(event, options){
+            options.lineHtml.classList.remove('current-subtitle');
+        },
+        frame: function(event, options) {
+
+        },
+        toString: function(event, options) {
+
+        } 
+    });
+})(Popcorn);
