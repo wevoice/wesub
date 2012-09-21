c772fbeef7af0196f599957463c5ace32f4812bc
Arthur Debert <arthur@stimuli.com.br>
2012-09-21 13:31:54 -0300

    Fixing more hard coded URLS for the new embedder

--- media/src/js/embedder/embedder-dev.js
+++ media/src/js/embedder/embedder-dev.js
@@ -11750,9 +11750,6 @@ Popcorn.plugin('amarasubtitle', {
         }
     });
 })(Popcorn);
-/* This is generated during media compilation. It's added to the repo, but on gitignore, as 
- * new compiles will overwrite the file with the environment specific data.
- */
 _amaraConf = {
     'baseURL': 'unisubs.example.com:8000',
     'staticURL':'http://unisubs.example.com:8000/site_media/static-cache/852cffe9/'
@@ -12499,7 +12496,7 @@ _amaraConf = {
             style.rel = 'stylesheet';
             style.type = 'text/css';
 
-            style.href = '/site_media/src/css/embedder/embedder-dev.css';
+            style.href = _amaraConf.staticURL + 'release/public/embedder.css';
             tag.parentNode.insertBefore(style, tag);
 
             // Change the template delimiter for Underscore templates.
