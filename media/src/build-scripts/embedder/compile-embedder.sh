ff8ab0dc463b5d6524390d955c141e983805fa18
Nick Sergeant <nick@nicksergeant.com>
2012-09-21 16:27:05 -0400

    Remove embedder-dev.css and embedder-dev.js. They'll be in media/release now upon local compilation (untracked). Update compile-embedder. Add conf-dev for local compilation.

--- media/src/build-scripts/embedder/compile-embedder.sh
+++ media/src/build-scripts/embedder/compile-embedder.sh
@@ -9,20 +9,8 @@ cat media/src/js/third-party/json2.min.js \
     media/src/js/third-party/popcorn.js \
     media/src/js/embedder/popcorn.amaratranscript.js \
     media/src/js/embedder/popcorn.amarasubtitle.js \
-    media/src/js/embedder/conf.js \
+    media/src/js/embedder/conf-dev.js \
     media/src/js/embedder/embedder.js \
-  > media/src/js/embedder/embedder-dev.js
+  > media/release/public/embedder.js
 
-scss -t compressed media/src/css/embedder/embedder.scss media/src/css/embedder/embedder-dev.css
-
-cp media/src/js/embedder/embedder-dev.js media/release/public/embedder.js
-cp media/src/css/embedder/embedder-dev.css media/release/public/embedder.css
-echo 'a'
-sed -i -e 's/embedder-dev/embedder/g' media/release/public/embedder.js
-echo 'b'
-sed -i -e 's/\/site_media\/src\/css\//\/\/s3.amazonaws.com\/s3.www.universalsubtitles.org\/release\/public\//g' media/release/public/embedder.js
-sed -i -e 's/\/site_media\/images\//\/\/s3.amazonaws.com\/s3.www.universalsubtitles.org\/site_media\/images\//g' media/release/public/embedder.css
-
-
-rm media/release/public/embedder.js-e
-rm media/release/public/embedder.css-e
+scss -t compressed media/src/css/embedder/embedder.scss media/release/public/embedder.css
