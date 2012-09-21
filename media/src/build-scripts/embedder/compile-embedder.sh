9314be9968f37a81c976ed7a0a65e965df56256f
Nick Sergeant <nick@nicksergeant.com>
2012-09-21 16:33:46 -0400

    Adding embedder-dev.css back to its original place, as the compiler needs it.

--- media/src/build-scripts/embedder/compile-embedder.sh
+++ media/src/build-scripts/embedder/compile-embedder.sh
@@ -13,4 +13,7 @@ cat media/src/js/third-party/json2.min.js \
     media/src/js/embedder/embedder.js \
   > media/release/public/embedder.js
 
-scss -t compressed media/src/css/embedder/embedder.scss media/release/public/embedder.css
+scss -t compressed media/src/css/embedder/embedder.scss media/src/css/embedder/embedder-dev.css
+
+# This is just for local development.
+cp media/src/css/embedder/embedder-dev.css media/release/public/embedder.css
