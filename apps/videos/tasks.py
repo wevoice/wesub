b5d55dc9e56db88feae341ddbed622d09961fff3
Fernando Takai <fernando.takai@gmail.com>
2012-09-25 14:58:30 -0300

    Removed unused gauge and changed subtitlelanguage to newsubtitlelanguagews_set

--- apps/videos/tasks.py
+++ apps/videos/tasks.py
@@ -488,11 +488,6 @@ def _save_video_feed(feed_url, last_entry_url, user):
 @periodic_task(run_every=timedelta(seconds=5))
 def gauge_videos():
     Gauge('videos.Video').report(Video.objects.count())
-    Gauge('videos.Video-captioned').report(Video.objects.exclude(subtitlelanguage=None).count())
+    Gauge('videos.Video-captioned').report(Video.objects.exclude(newsubtitlelanguage_set=None).count())
     Gauge('videos.SubtitleVersion').report(SubtitleVersion.objects.count())
     Gauge('videos.SubtitleLanguage').report(SubtitleLanguage.objects.count())
-
-@periodic_task(run_every=timedelta(seconds=(60*5)))
-def gauge_videos_long():
-    Gauge('videos.Subtitle').report(Subtitle.objects.count())
-
