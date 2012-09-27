41104bba0016d4a346b86d5fa7c5e420c055531e
Fernando Takai <fernando.takai@gmail.com>
2012-09-27 14:45:41 -0300

    Fixed alarms to use version's video property

--- apps/videos/alarms.py
+++ apps/videos/alarms.py
@@ -29,7 +29,7 @@ def check_other_languages_changes(version, ignore_statistic=False):
         return
     
     if not ignore_statistic:
-        fetch_count = SubtitleFetchCounters.objects.filter(video=version.language.video, date=date.today()) \
+        fetch_count = SubtitleFetchCounters.objects.filter(video=version.video, date=date.today()) \
             .aggregate(fetch_count=Count('count'))['fetch_count']
         
         if fetch_count < 100:
@@ -37,17 +37,17 @@ def check_other_languages_changes(version, ignore_statistic=False):
     
     d = datetime.now() - timedelta(hours=1)
     
-    changed_langs_count = SubtitleLanguage.objects.filter(video=version.language.video) \
+    changed_langs_count = SubtitleLanguage.objects.filter(video=version.video) \
         .filter(subtitleversion__datetime_started__gte=d).count()
         
-    all_langs_count = SubtitleLanguage.objects.filter(video=version.language.video).count()
+    all_langs_count = SubtitleLanguage.objects.filter(video=version.video).count()
     
     if (float(changed_langs_count) / all_langs_count) >= 0.5:
         send_alarm_email(version, u'A video had changes made two more than 50% of its languages in the last hour')
 
 def check_language_name(version, ignore_statistic=False):
     if not ignore_statistic:
-        fetch_count = SubtitleFetchCounters.objects.filter(video=version.language.video, date=date.today()) \
+        fetch_count = SubtitleFetchCounters.objects.filter(video=version.video, date=date.today()) \
             .aggregate(fetch_count=Count('count'))['fetch_count']
         
         if fetch_count < 100:
