79525401189c1b394a0a01e6730cda87e0840046
Steve Losh <steve@stevelosh.com>
2012-10-29 15:26:35 -0400

    Pull video model tests out into their own file.

--- apps/videos/tests/__init__.py
+++ apps/videos/tests/__init__.py
@@ -2,9 +2,10 @@ from apps.videos.tests.celery_tasks import *
 from apps.videos.tests.downloads import *
 from apps.videos.tests.feeds import *
 from apps.videos.tests.following import *
-from apps.videos.tests.uploads import *
+from apps.videos.tests.models import *
 from apps.videos.tests.syncing import *
 from apps.videos.tests.template_tags import *
+from apps.videos.tests.uploads import *
 from apps.videos.tests.videos import *
 from apps.videos.tests.video_types import *
 from apps.videos.tests.video_urls import *
--- apps/videos/tests/videos.py
+++ apps/videos/tests/videos.py
@@ -34,9 +34,7 @@ from apps.videos.models import (
     Video, Action, SubtitleLanguage, Subtitle, SubtitleVersion
 )
 from apps.videos.rpc import VideosApiClass
-from apps.videos.tasks import video_changed_tasks
 from apps.videos.types import video_type_registrar
-from apps.widget import video_cache
 from apps.widget.rpc import Rpc
 from apps.widget.tests import RequestMockup
 
@@ -187,43 +185,6 @@ class WebUseTest(TestCase):
         self.client.login(**self.auth)
 
 
-class VideoTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.all()[0]
-        self.youtube_video = 'http://www.youtube.com/watch?v=pQ9qX8lcaBQ'
-        self.html5_video = 'http://mirrorblender.top-ix.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_stereo.ogg'
-
-    def test_video_create(self):
-        self._create_video(self.youtube_video)
-        self._create_video(self.html5_video)
-
-    def _create_video(self, video_url):
-        video, created = Video.get_or_create_for_url(video_url)
-        self.failUnless(video)
-        self.failUnless(created)
-        more_video, created = Video.get_or_create_for_url(video_url)
-        self.failIf(created)
-        self.failUnlessEqual(video, more_video)
-
-    def test_video_cache_busted_on_delete(self):
-        start_url = 'http://videos.mozilla.org/firefox/3.5/switch/switch.ogv'
-        video, created = Video.get_or_create_for_url(start_url)
-        video_url = video.get_video_url()
-        video_pk = video.pk
-
-        cache_id_1 = video_cache.get_video_id(video_url)
-        self.assertTrue(cache_id_1)
-        video.delete()
-        self.assertEqual(Video.objects.filter(pk=video_pk).count() , 0)
-        # when cache is not cleared this will return arn
-        cache_id_2 = video_cache.get_video_id(video_url)
-        self.assertNotEqual(cache_id_1, cache_id_2)
-        # create a new video with the same url, has to have same# key
-        video2, created= Video.get_or_create_for_url(start_url)
-        video_url = video2.get_video_url()
-        cache_id_3 = video_cache.get_video_id(video_url)
-        self.assertEqual(cache_id_3, cache_id_2)
-
 class RpcTest(TestCase):
     fixtures = ['test.json']
 
@@ -245,69 +206,6 @@ class RpcTest(TestCase):
         except Action.DoesNotExist:
             self.fail()
 
-
-class TestModelsSaving(TestCase):
-
-    fixtures = ['test.json', 'subtitle_fixtures.json']
-
-    def setUp(self):
-        self.video = Video.objects.all()[:1].get()
-        self.language = self.video.subtitle_language()
-
-    def test_video_languages_count(self):
-        #test if fixtures has correct data
-        langs_count = self.video.newsubtitlelanguage_set.having_nonempty_tip().count()
-
-        self.assertEqual(self.video.languages_count, langs_count)
-        self.assertTrue(self.video.languages_count > 0)
-
-        self.video.languages_count = 0
-        self.video.save()
-        video_changed_tasks.delay(self.video.pk)
-        self.video = Video.objects.get(id=self.video.id)
-        self.assertEqual(self.video.languages_count, langs_count)
-
-    def test_subtitle_language_save(self):
-        self.assertEqual(self.video.complete_date, None)
-        self.assertEqual(self.video.subtitlelanguage_set.count(), 1)
-
-        self.language.subtitles_complete = True
-        self.language.save()
-        video_changed_tasks.delay(self.video.pk)
-        self.video = Video.objects.get(pk=self.video.pk)
-        self.assertNotEqual(self.video.complete_date, None)
-
-        self.language.subtitles_complete = False
-        self.language.save()
-        video_changed_tasks.delay(self.video.pk)
-        self.video = Video.objects.get(pk=self.video.pk)
-        self.assertEqual(self.video.complete_date, None)
-
-        version = create_langs_and_versions(self.video, ['ru'])
-        new_language = version[0].subtitle_language
-        new_language.subtitles_complete = True
-        new_language.save()
-        video_changed_tasks.delay(self.video.pk)
-        self.video = Video.objects.get(pk=self.video.pk)
-        self.assertNotEqual(self.video.complete_date, None)
-
-        self.language.subtitles_complete = True
-        self.language.save()
-        video_changed_tasks.delay(self.video.pk)
-        self.video = Video.objects.get(pk=self.video.pk)
-        self.assertNotEqual(self.video.complete_date, None)
-
-        new_language.subtitles_complete = False
-        new_language.save()
-        video_changed_tasks.delay(self.video.pk)
-        self.video = Video.objects.get(pk=self.video.pk)
-        self.assertNotEqual(self.video.complete_date, None)
-
-        self.language.subtitles_complete = False
-        self.language.save()
-        video_changed_tasks.delay(self.video.pk)
-        self.video = Video.objects.get(pk=self.video.pk)
-        self.assertEqual(self.video.complete_date, None)
 
 class TestVideoForm(TestCase):
     def setUp(self):
@@ -344,7 +242,6 @@ class TestVideoForm(TestCase):
 
     def test_dailymotion_urls(self):
         self._test_urls(self.daily_motion_urls)
-
 
 
 class TestMetadataManager(TestCase):
