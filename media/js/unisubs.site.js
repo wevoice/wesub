8bbe7bb8445cdb1e8cd31501c3d4fd8551e8783b
Nick Sergeant <nick@nicksergeant.com>
2012-11-12 11:42:47 -0500

    Don't pre-select *any* project on TED team's task view.

--- media/js/unisubs.site.js
+++ media/js/unisubs.site.js
@@ -171,12 +171,6 @@ var Site = function(Site) {
              */
             if (window.TEAM_SLUG == 'ted') {
 
-                // If this is a task view for an assignee, select the 'any project'
-                // option.
-                if (window.ASSIGNEE) {
-                    return $('option[id="project-opt-any"]');
-                }
-
                 // If this is a video's task listing, select the 'any project'
                 // option.
                 if (window.REQUEST_TEAM_VIDEO && window.REQUEST_TEAM_VIDEO !==  '') {
