331ee42cace32e45c3aac3b06afd3e2875bbcc1e
Arthur Debert <arthur@stimuli.com.br>
2012-12-13 12:19:50 -0200

    Initial view for the new editor, some structure to the angular subtitle service + directives (doesn't work as is).

--- apps/subtitles/views.py
+++ apps/subtitles/views.py
@@ -15,25 +15,97 @@
 #
 # You should have received a copy of the GNU Affero General Public License along
 # with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html.
-from videos.models import Video
-from subtitles.models import SubtitleLanguage
+import json
 
+from videos.models import Video
+from subtitles.models import SubtitleLanguage, SubtitleVersion
+
+from django.db.models import Count
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 
+def _version_data(version):
+    '''
+    Creates a dict with version info, suitable for encoding
+    into json and bootstrapping the editor.
+    '''
+    return {
+        'number': version.version_number,
+        'subtitlesXML': version.get_subtitles().to_xml(),
+        'title': version.title,
+        'description': version.description,
+    }
+
+def _language_data(language, editing_version, translated_from_version):
+    '''
+    Creates a dict with language info, suitable for encoding
+    into json and bootstrapping the editor. Includes
+    the version data for the version being edited and the
+    original translation source, if any.
+    '''
+    versions_data = []
+    for version_number in range(1, language.num_versions +1):
+        version_data = {'number':version_number}
+        if editing_version and editing_version.version_number == version_number and \
+            editing_version.subtitle_language == language:
+            version_data.update(_version_data(editing_version))
+        elif translated_from_version and translated_from_version.version_number == version_number and \
+            translated_from_version.subtitle_language == language:
+            version_data.update(_version_data(translated_from_version))
+
+        versions_data.append(version_data)
+    return {
+        'translatedFrom': translated_from_version and {
+            'language_code': translated_from_version.subtitle_language.language_code,
+            'version_number': translated_from_version.version_number,
+        },
+        'editingLanguage': language == editing_version.subtitle_language,
+        'code': language.language_code,
+        'name': language.get_language_code_display(),
+        'pk': language.pk,
+        'numVersions': language.num_versions,
+        'versions': versions_data,
+    }
+
 def subtitle_editor(request, video_id, language_code, task_id=None):
-    # FIXME permissions the motherf
+    '''
+    Renders the subtitle-editor page, with all data neeeded for the UI
+    as a json object on the html document.
+    If the language does not exist, it will create one and lock it.
+    Also decides what source version should be shown initially (if
+    it is a translation).
+    '''
+    # FIXME: permissions
     video = get_object_or_404(Video, video_id=video_id)
     # FIXME: validate language code
     try:
-        language = video.newsubtitlelanguage_set.get(language_code=language_code)
+        editing_language = video.newsubtitlelanguage_set.get(language_code=language_code)
     except SubtitleLanguage.DoesNotExist:
-        language = SubtitleLanguage(video=video,language_code=language_code )
-    if not language.can_writelock(request.browser_id):
+        editing_language = SubtitleLanguage(video=video,language_code=language_code )
+    if not editing_language.can_writelock(request.browser_id):
         return render_to_response("subtitles/subtitle-editor-locked.html")
-    language.writelock(request.user, request.browser_id, save=True)
+    editing_language.writelock(request.user, request.browser_id, save=True)
+    # if this language is a traslation, show both
+    editing_version = editing_language.get_tip(public=False)
+    translated_from_version = None
+    lineage = editing_version and editing_version.get_lineage()
+    if editing_version and lineage:
+        translated_from_version = SubtitleVersion.objects.get(
+            subtitle_language__language_code=lineage.keys()[0], version_number=lineage.values()[0])
+    languages = video.newsubtitlelanguage_set.having_nonempty_versions().annotate(
+        num_versions=Count('subtitleversion'))
+    editor_data = {
+        'video': {
+            'id': video.video_id,
+            'videoURL': video.get_video_url()
+        },
+        'languages': [_language_data(lang, editing_version, translated_from_version) for lang in languages],
+    }
     return render_to_response("subtitles/subtitle-editor.html", {
         'video': video,
-        'language': language
+        'language': editing_language,
+        'other_languages': languages,
+        'version': editing_version,
+        'editor_data': json.dumps(editor_data, indent=4)
     }, context_instance=RequestContext(request))
 
--- media/src/js/subtitle-editor/editor-app.js
+++ media/src/js/subtitle-editor/editor-app.js
@@ -15,3 +15,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see
 // http://www.gnu.org/licenses/agpl-3.0.html.
+angular.module('amara.SubtitleEditor', [
+    'amara.SubtitleEditor.services'
+]);
