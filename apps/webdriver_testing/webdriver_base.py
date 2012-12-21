b81161c3f989f14ea401362c4fd865f1600ca986
Janet <jed@pculture>
2012-12-21 10:34:49 +0100

    webdriver_base.py

--- apps/webdriver_testing/webdriver_base.py
+++ apps/webdriver_testing/webdriver_base.py
@@ -67,7 +67,8 @@ class WebdriverTestCase(LiveServerTestCase, TestCase):
         if settings.VAGRANT_VM:             
             self.base_url = 'http://unisubs.example.com:80/'
         else:
-            self.base_url = self.live_server_url + '/'
+            #self.base_url = self.live_server_url + '/'
+            self.base_url = 'http://dev.universalsubtitles.org:8101/'
 
         #If we are using sauce need to use DesiredCapabilities for browser config.
         #The env variables can be set via the jenkins plugin if we are using it.
