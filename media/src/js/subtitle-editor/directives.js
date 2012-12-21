0f786f2025efc96e0379b049bc8e8168bbb02f9d
Honza Pokorny <me@honza.ca>
2012-12-21 09:12:07 -0400

    Specify jquery for the directives file.

--- media/src/js/subtitle-editor/directives.js
+++ media/src/js/subtitle-editor/directives.js
@@ -1,40 +1,44 @@
-var directives = angular.module("amara.SubtitleEditor.directives", []);
-directives.directive("subtitleList", function(SubtitleFetcher){
-    var $ = window.AmarajQuery;
-    var isEditable ;
-    var selectedScope, selectedController;
-    function onItemClicked(e){
-        var controller = angular.element(e.srcElement).controller();
-        var scope = angular.element(e.srcElement).scope();
-        // make sure the user clicked on the list item
-        if (controller instanceof SubtitleListItemController){
-            if (selectedScope){
-                selectedScope.finishEditingMode();
-            }
-            selectedScope = scope;
-            selectedScope.startEditingMode();
+(function($) {
 
-        }
-    }
-    return {
+    var directives = angular.module("amara.SubtitleEditor.directives", []);
 
-        link: function link(scope, elm, attrs){
-            scope.getSubtitles(attrs.languageCode, attrs.versionNumber);
+    directives.directive("subtitleList", function(SubtitleFetcher){
+        var isEditable, selectedScope, selectedController;
+        function onItemClicked(e){
+            var controller = angular.element(e.srcElement).controller();
+            var scope = angular.element(e.srcElement).scope();
+            // make sure the user clicked on the list item
+            if (controller instanceof SubtitleListItemController){
+                if (selectedScope){
+                    selectedScope.finishEditingMode();
+                }
+                selectedScope = scope;
+                selectedScope.startEditingMode();
 
-            isEditable = attrs.editable === 'true';
-            // if is editable, hook up event listeners
-            if (isEditable){
-                $(elm).click(onItemClicked);
             }
         }
-    };
+        return {
 
-});
-directives.directive("subtitleListItem", function(SubtitleFetcher){
-    return {
-        link: function link(scope, elm, attrs){
+            link: function link(scope, elm, attrs){
+                scope.getSubtitles(attrs.languageCode, attrs.versionNumber);
 
-        }
-    };
+                isEditable = attrs.editable === 'true';
+                // if is editable, hook up event listeners
+                if (isEditable){
+                    $(elm).click(onItemClicked);
+                }
+            }
+        };
 
-});
+    });
+
+    directives.directive("subtitleListItem", function(SubtitleFetcher){
+        return {
+            link: function link(scope, elm, attrs){
+
+            }
+        };
+
+    });
+
+})(window.AmarajQuery);
