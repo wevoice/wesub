4bd1a80a2a13ecdde662946c59fc9522c0424fe7
Janet <jed@pculture>
2012-12-28 11:37:40 +0100

    use normal setup.

--- apps/webdriver_testing/webdriver_base.py
+++ apps/webdriver_testing/webdriver_base.py
@@ -24,106 +24,40 @@ from selenium import webdriver
 from django.conf import settings
 from apps.webdriver_testing.data_factories import UserFactory
 
+
 class WebdriverTestCase(LiveServerTestCase, TestCase):
     def setUp(self):
         super(WebdriverTestCase, self).setUp()
-
-        """This is where we want to setup the browser configuation.
-
-        If you want to use something other than default (no sauce on ff)
-        then it should be setup as env vars in the system under test. When
-        running sauce on jenkins with the jenkins pluging - then the vars are
-        set there.
-        
-        Environment vars recognized by jenkins sauce plugin.
-        SELENIUM_HOST - The hostname of the Selenium server
-        SELENIUM_PORT - The port of the Selenium server
-        SELENIUM_PLATFORM - The operating system of the selected browser
-        SELENIUM_VERSION - The version number of the selected browser
-        SELENIUM_BROWSER - The browser string.
-        SELENIUM_URL - The initial URL to load when the test begins
-        SAUCE_USER_NAME - The user name used to invoke Sauce OnDemand
-        SAUCE_API_KEY - The access key for the user used to invoke Sauce OnDemand
-        36114ded-d388-4ee0-92f7-d82d8530ff3e
-
-        We are going to look for a USE_SAUCE = True if we are using sauce, 
-        and a default browser TEST_BROWSER if not using sauce.
-        """
-        self.use_sauce = os.environ.get('USE_SAUCE', False)
-        if self.use_sauce and settings.VAGRANT_VM:
-            raise Exception("Can not run against sauce labs via vagrant vm")
-
+  
         LiveServerTestCase.setUp(self)
-
-
-        # Get rid of the previous screenshot if we aren't using sauce
-        if not self.use_sauce:
-            try: 
-                os.unlink('apps/webdriver_testing/Results/%s.png' % self.id())
-            except:
-                pass
+        try:  # Get rid of the previous screenshot
+            os.unlink('apps/webdriver_testing/Results/%s.png' % self.id())
+        except:
+            pass
 
         #if running in vagrant VM, must use port 80 for headless browser
         if settings.VAGRANT_VM:             
             self.base_url = 'http://unisubs.example.com:80/'
         else:
-            self.base_url = (self.live_server_url + '/').replace('localhost', 'dev.universalsubtitles.org')
-            print self.base_url
-        #If we are using sauce need to use DesiredCapabilities for browser config.
-        #The env variables can be set via the jenkins plugin if we are using it.
-        if self.use_sauce:
+            self.base_url = self.live_server_url + '/'
 
-            #FIXME Key set for test purposes only - remove and update when done.
-            sauce_key = os.environ.get('SAUCE_API_KEY', 
-                                       '36114ded-d388-4ee0-92f7-d82d8530ff3e')
-            jenkins_ws = os.environ.get('WORKSPACE', False)
-               
-            test_browser = os.environ.get('SELENIUM_BROWSER', 'CHROME')
-            dc = getattr(webdriver.DesiredCapabilities, test_browser) 
-            dc['version'] = os.environ.get('SELENIUM_VERSION', '')
-            dc['platform'] = os.environ.get('SELENIUM_PLAFORM', 'WINDOWS 2008')
-            dc['name'] = self.shortDescription()
-
-            #Setup the remote browser capabilities
-            self.browser = webdriver.Remote(
-                desired_capabilities = dc,
-                command_executor=("http://jed-pcf:%s@ondemand.saucelabs.com:80"
-                                  "/wd/hub" % sauce_key) 
-                                  )
-            self.browser.implicitly_wait(2)
-
-        #Otherwise just running locally - setup the browser to use.
-        else:
-            test_browser = os.environ.get('TEST_BROWSER', 'Firefox')
-            self.browser = getattr(webdriver, test_browser)()
-            self.browser.implicitly_wait(1)
-
-
-        #Open your browser to the test site. 
+        # BROWSER TO USE FOR TESTING - you can set TEST_BROWSER via os env to use 
+        # Chrome in place of the Firefox default.
+        test_browser = os.environ.get('TEST_BROWSER', 'Firefox')
+        self.browser = getattr(webdriver, test_browser)()
         self.browser.get(self.base_url)
-        self.browser.implicitly_wait(1)
+        #self.browser.implicitly_wait(1)
 
-        # Create a default admin user.
         UserFactory.create(username='admin', is_staff=True, is_superuser=True)
         self.auth = dict(username='admin', password='password')
-
-
         
+
     def tearDown(self):
-        if self.use_sauce:
-            print("\nLink to your job: https://saucelabs.com/jobs/%s" % self.browser.session_id)
-        else:
-            try:  #To get a screenshot of the last page and save to Results.
-                screenshot_file = ('apps/webdriver_testing/' 
-                                  'Results/%s.png' % self.id())
-                self.browser.get_screenshot_as_file(screenshot_file)
-            except: #But don't panic if fails, sometimes a timing thing, try again.
-                time.sleep(2)
-                self.browser.get_screenshot_as_file(screenshot_file)
-
-            try: #To quit the browser
-                self.browser.quit()
-            except: #But don't worry if you can't it may already be quit.
-                pass
-
+        try:  #To get a screenshot of the last page and save to Results.
+            time.sleep(2) #sometimes needs some extra time.
+            screenshot_file = ('apps/webdriver_testing/' 
+                              'Results/%s.png' % self.id())
+            self.browser.get_screenshot_as_file(screenshot_file)
+        finally: #But don't panic if fails
+            self.browser.quit()
 
