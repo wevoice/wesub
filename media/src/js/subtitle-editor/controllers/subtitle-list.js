02ca84a74f662069ba3b19d7e1af89623f398e83
Honza Pokorny <me@honza.ca>
2013-01-02 15:08:39 -0400

    Implement adding and removing of subs

--- media/src/js/subtitle-editor/controllers/subtitle-list.js
+++ media/src/js/subtitle-editor/controllers/subtitle-list.js
@@ -96,6 +96,19 @@
                                           this.dfxpWrapper.xmlToString(true, true));
             $scope.status = 'saving';
         };
+
+        $scope.addSubtitle = function(subtitle, index) {
+            if (subtitle.index !== index) {
+                throw Error("Indexes don't match.");
+            }
+
+            $scope.subtitlesData.splice(index, 0, subtitle);
+        };
+
+        $scope.removeSubtitle = function(index) {
+            $scope.subtitlesData.splice(index, 1);
+        };
+
     };
 
     /**
