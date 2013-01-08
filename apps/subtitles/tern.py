36a5dd19256802a9f623bffc0fd04bfae21b9d4d
Steve Losh <steve@stevelosh.com>
2013-01-08 14:02:36 -0500

    tern: log and skip duplicate languages for now

--- apps/subtitles/tern.py
+++ apps/subtitles/tern.py
@@ -202,6 +202,10 @@ def _create_subtitle_language(sl):
     """Sync the given subtitle language, creating a new one."""
     from apps.subtitles.models import SubtitleLanguage as NewSubtitleLanguage
 
+    if NewSubtitleLanguage.objects.filter(video=sl.video).exists():
+        log('SubtitleLanguage', 'ERROR_DUPLICATE_LANGUAGE', sl.pk, None)
+        return
+
     nsl = NewSubtitleLanguage(
         video=sl.video,
         language_code=sl.language,
@@ -268,7 +272,7 @@ def _sync_language(language_pk=None):
         return False
 
     if sl.language == '':
-        log('SubtitleLanguage', 'BAD_LANGUAGE', sl.pk, None)
+        log('SubtitleLanguage', 'ERROR_EMPTY_LANGUAGE', sl.pk, None)
         return True
 
     if sl.new_subtitle_language:
