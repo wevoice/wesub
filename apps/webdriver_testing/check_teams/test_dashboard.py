c5a5aa142996f720e5ba8b6f1db0d23440f2cb04
Janet <jed@pculture>
2013-01-21 07:13:05 +0100

    specify data for vid create

--- apps/webdriver_testing/check_teams/test_dashboard.py
+++ apps/webdriver_testing/check_teams/test_dashboard.py
@@ -34,10 +34,10 @@ class TestCaseTaskFreeDashboard(WebdriverTestCase):
                        ('fireplace.mp4', 'en', 'en')
                        ]
         for vid in test_videos:
-            video = cls.data_utils.create_video(url = 'http://qa.pculture.org/amara_tests/'
-                                                '%s' % vid[0])
-            video.title = vid[0]
-            video.save()
+            vidurl_data = {'url': 'http://qa.pculture.org/amara_tests/%s' % vid[0],
+                           'video__title': vid[0]}
+
+            video = cls.data_utils.create_video(**vidurl_data)
 
             if vid[1] is not None:
                 video_data = {'language_code': vid[1],
@@ -265,11 +265,10 @@ class TestCaseTasksEnabledDashboard(WebdriverTestCase):
                        ]
         cls.vid_obj_list = []
         for vid in test_videos:
-            
-            video = cls.data_utils.create_video(url = 'http://qa.pculture.org/amara_tests/'
-                                                 '%s' % vid[0])
-            video.title = vid[0]
-            video.save()
+            vidurl_data = {'url': 'http://qa.pculture.org/amara_tests/%s' % vid[0],
+                           'video__title': vid[0]}
+
+            video = cls.data_utils.create_video(**vidurl_data)
             if vid[1] is not None:
                 video.primary_audio_language_code = vid[1]
                 video.save()
