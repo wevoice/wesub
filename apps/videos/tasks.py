cec2d10d52ba2d75a0c018a47bdc727d2355f57f
Honza Pokorny <me@honza.ca>
2013-01-29 16:52:58 -0400

    Catch VideoTypeErrors when updating YT descs

--- apps/videos/tasks.py
+++ apps/videos/tasks.py
@@ -40,6 +40,7 @@ from videos.models import (
     VIDEO_TYPE_YOUTUBE, VideoUrl
 )
 from videos.types import video_type_registrar
+from videos.types.base import VideoTypeError
 from videos.feed_parser import FeedParser
 
 celery_logger = logging.getLogger('celery.task')
@@ -540,7 +541,13 @@ def _add_amara_description_credit_to_youtube_vurl(vurl_pk):
             'vurl_pk': vurl_pk})
         return
 
-    vt = video_type_registrar.video_type_for_url(vurl.url)
+    try:
+        vt = video_type_registrar.video_type_for_url(vurl.url)
+    except VideoTypeError, e:
+        celery_logger.warning("Video type error", extra={
+            "exception_thrown": str(e)})
+        return
+
     try:
         account = ThirdPartyAccount.objects.get(username=vurl.owner_username)
     except ThirdPartyAccount.DoesNotExist:
