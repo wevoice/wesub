076f3d367ffb391509f7bbbd0166c0d096b3d520
Craig Zheng <craig.zheng@gmail.com>
2013-02-13 13:37:09 -0500

    Resolve merge conflicts betw prod and i-1866

--- apps/accountlinker/tasks.py
+++ apps/accountlinker/tasks.py
@@ -1,9 +1,11 @@
 import logging
-from celery.task import task
-from videos.models import VideoUrl
+from celery.task import task, periodic_task
+from celery.schedules import timedelta
+from videos.models import VideoUrl, VIDEO_TYPE_YOUTUBE
 from videos.types import UPDATE_VERSION_ACTION
 from auth.models import CustomUser as User
 from models import ThirdPartyAccount
+from utils.metrics import Gauge
 
 
 logger = logging.getLogger(__name__)
@@ -24,7 +26,7 @@ def get_youtube_data(user_pk):
         logger.error('User with PK %s not found' % user_pk)
         return
 
-    usernames = [a.username for a in user.third_party_accounts.all()]
+    usernames = [a.full_name for a in user.third_party_accounts.all()]
     urls = VideoUrl.objects.filter(owner_username__in=usernames)
     videos = [url.video for url in urls]
 
@@ -52,3 +54,9 @@ def mirror_existing_youtube_videos(user_pk):
     for video, language, version in data:
         ThirdPartyAccount.objects.mirror_on_third_party(video, language,
                 UPDATE_VERSION_ACTION, version)
+
+
+@periodic_task(run_every=timedelta(seconds=60))
+def gauge_videos():
+    count = ThirdPartyAccount.objects.filter(type=VIDEO_TYPE_YOUTUBE).count()
+    Gauge('youtube.accounts_linked').report(count)
