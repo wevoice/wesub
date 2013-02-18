0eb1049f36326e7ea94825cdf765e8df67e4fa22
Arthur Debert <arthur@stimuli.com.br>
2013-02-18 13:31:58 -0300

    Still removing the demo, sigh, there were two declarations on urls.py

--- apps/videos/urls.py
+++ apps/videos/urls.py
@@ -40,7 +40,6 @@ urlpatterns = patterns(
     url(r'^create/$', 'create', name='create'),
     url(r'^create/feed/$', 'create_from_feed', name='create_from_feed'),
     url(r'^email_friend/$', 'email_friend', name='email_friend'),
-    url(r'^demo/$', 'demo', name='demo'),
     url(r'^activities/(?P<video_id>(\w|-)+)/$', 'actions_list', name='actions_list'),
     url(r'^stop_notification/(?P<video_id>(\w|-)+)/$', 'stop_notification', name='stop_notification'),
     url(r'^(?P<video_id>(\w|-)+/)?rollback/(?P<pk>\d+)/$', 'rollback', name='rollback'),
