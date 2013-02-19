2d014859bbb1b762759a10a52f84b34687c9fc6a
Steve Losh <steve@stevelosh.com>
2013-02-19 14:18:08 -0500

    1974: fork translations when uploading standalone subs

--- apps/subtitles/forms.py
+++ apps/subtitles/forms.py
@@ -24,6 +24,7 @@ from django.utils.encoding import force_unicode
 from django.utils.translation import ugettext_lazy as _
 
 from apps.subtitles import pipeline
+from apps.subtitles.shims import is_dependent
 from apps.teams.permissions import (
     can_assign_task, can_create_and_edit_subtitles,
     can_create_and_edit_translations
@@ -344,6 +345,15 @@ class SubtitlesUploadForm(forms.Form):
             self.video, language_code, self._parsed_subtitles,
             title=title, description=description, author=self.user,
             parents=parents, committer=self.user, complete=complete)
+
+        # Handle forking SubtitleLanguages that were translations when
+        # a standalone version is uploaded.
+        #
+        # For example: assume there is a French translation of English.
+        # Uploading a "straight from video" version of French should fork it.
+        if not from_language_code and is_dependent(version.subtitle_language):
+            version.subtitle_language.is_forked = True
+            version.subtitle_language.save()
 
         # TODO: Pipeline this.
         video_changed_tasks.delay(version.subtitle_language.video_id,
