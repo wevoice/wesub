1941168a22de2df84935e9857f2946b076660bc2
Arthur Debert <arthur@stimuli.com.br>
2013-03-08 15:15:27 -0300

    2099: Fixed multiple subtitles on top of video

--- media/src/js/popcorn/popcorn.amarasubtitle.js
+++ media/src/js/popcorn/popcorn.amarasubtitle.js
@@ -3,12 +3,11 @@ var Popcorn = Popcorn || null;
 (function (Popcorn) {
 
     var i = 0;
-    var createDefaultContainer = function(context, id) {
+    var newdiv;
 
+    var createDefaultContainer = function(context, id) {
         var ctxContainer = context.container = document.createElement('div');
         var style = ctxContainer.style;
-        var media = context.media;
-
         ctxContainer.id = id || Popcorn.guid();
         ctxContainer.className = 'amara-popcorn-subtitles';
 
@@ -16,51 +15,36 @@ var Popcorn = Popcorn || null;
 
         context.media.parentNode.appendChild(ctxContainer);
 
+        newdiv = document.createElement('div');
+        newdiv.id = Popcorn.guid('subtitle-');
+        newdiv.style.display = 'none';
+        ctxContainer.appendChild(newdiv);
         return ctxContainer;
     };
 
     Popcorn.plugin('amarasubtitle', {
         _setup: function(options) {
-            var newdiv = document.createElement('div');
-
-            newdiv.id = 'subtitle-' + i++;
-            newdiv.style.display = 'none';
-
             // Creates a div for all subtitles to use
             if (!this.container && (!options.target || options.target === 'subtitle-container')) {
                 createDefaultContainer(this);
             }
-
-            // if a target is specified, use that
-            if (options.target && options.target !== 'subtitle-container') {
-                // In case the target doesn't exist in the DOM
-                options.container = document.getElementById(options.target) || createDefaultContainer(this, options.target);
-            } else {
-                // use shared default container
-                options.container = this.container;
-            }
-            if (document.getElementById(options.container.id)) {
-                document.getElementById(options.container.id).appendChild(newdiv);
-            }
-            options.innerContainer = newdiv;
-
         },
         start: function(event, options){
             if (options.text !== '') {
-                options.innerContainer.style.display = 'inline-block';
-                options.innerContainer.innerHTML = options.text || '';
+                newdiv.style.display = 'inline-block';
+                newdiv.innerHTML = options.text || '';
             }
         },
         end: function(event, options) {
-            options.innerContainer.style.display = 'none';
+            newdiv.style.display = 'none';
         },
         _update: function(event, newOptions) {
-            event.innerContainer.innerHTML = event.text = newOptions.text;
+            newdiv.innerHTML = event.text = newOptions.text;
 
             if (newOptions.text === '') {
-                event.innerContainer.style.display = 'none';
+                newdiv.style.display = 'none';
             } else {
-                event.innerContainer.style.display = 'inline-block';
+                newdiv.style.display = 'inline-block';
             }
         },
         _teardown: function (options) {
