3e65d6ff49311a78d86701dd14ea75f463a9a45c
Janet <jed@pculture>
2013-03-20 15:39:39 +0100

    some more translations tests

--- apps/webdriver_testing/check_videos/test_upload_translations.py
+++ apps/webdriver_testing/check_videos/test_upload_translations.py
@@ -24,7 +24,7 @@ class TestCaseUploadTranslation(WebdriverTestCase):
 
         cls.subs_data_dir = os.path.join(os.getcwd(), 'apps', 
             'webdriver_testing', 'subtitle_data')
-        cls.video_pg.open_page('/')
+        cls.video_pg.open_page('')
         cls.video_pg.log_in(cls.user.username, 'password')
         cls.video_pg.set_skiphowto()
 
@@ -33,6 +33,7 @@ class TestCaseUploadTranslation(WebdriverTestCase):
         self.tv = self.data_utils.create_video()
         self.data_utils.upload_subs(self.tv)
         self.video_pg.open_video_page(self.tv.video_id)
+        self.video_pg.handle_js_alert('accept')
         
     def _upload_and_verify(self, tv, sub_file, language, lang_code):
         """Upload the subtitle file and confirm subs are stored.
@@ -41,13 +42,13 @@ class TestCaseUploadTranslation(WebdriverTestCase):
         value in the database for the latest version of the lang.
 
         """
-        self.video_pg.upload_subtitles(language, sub_file, 
+        message = self.video_pg.upload_subtitles(language, sub_file, 
                                        translated_from='English')
+        self.logger.info("MESSAGE: %s" %message)
         subtitle_lang = tv.subtitle_language(lang_code) 
         page_url = 'videos/{0}/{1}/'.format(tv.video_id, lang_code)
         self.video_pg.open_page(page_url)
         return subtitle_lang.get_subtitle_count()
-
 
 
     def test_new_translation(self):
@@ -58,7 +59,6 @@ class TestCaseUploadTranslation(WebdriverTestCase):
         sub_file = os.path.join(self.subs_data_dir, test_file)       
         sc = self._upload_and_verify(self.tv, sub_file, 'Swedish', 'sv')
         self.assertEqual(sc, 72)    
-
 
 
     def test_edit(self):
@@ -105,8 +105,56 @@ class TestCaseUploadTranslation(WebdriverTestCase):
                          sub_editor.dialog_title())
 
 
+    def test_txt(self):
+        """Upload translation (de) in a txt file.
 
+        """
+        test_file = 'Timed_text.en.txt'
+        sub_file = os.path.join(self.subs_data_dir, test_file)
+        sc = self._upload_and_verify(self.tv, sub_file, 'German', 'de')
+        self.assertEqual(sc, 72) 
 
+    def test_srt(self):
+        """Upload translation (sv) in a srt file.
 
+        """
+        sub_file = os.path.join(self.subs_data_dir, 'Timed_text.sv.srt')
+        sc = self._upload_and_verify(self.tv, sub_file, 'Swedish', 'sv')
+        self.assertEqual(sc, 72) 
+       
 
+    def test_sbv__more_lines(self):
+        """Can not upload dependant translation with more lines than source'.
 
+        """
+        sub_file = os.path.join(self.subs_data_dir, 'Timed_text.zh-cn.sbv')
+        message = self.video_pg.upload_subtitles('Chinese, Simplified', sub_file, 
+                                       translated_from='English')
+        self.assertEqual(message, ("Sorry, we couldn't upload your file "
+                                   "because the number of lines in your "
+                                   "translation (243) doesn't match the "
+                                   "original (72)."))
+
+    def test_ssa__less_lines(self):
+        """Can upload a dependant translation with less lines than source.
+
+        """
+        sub_file = os.path.join(self.subs_data_dir, 'less_lines.ssa')       
+        sc = self._upload_and_verify(self.tv, sub_file, 'Hungarian', 'hu')
+        self.assertEqual(sc, 7) 
+
+    def test_dfxp(self):
+        """Upload translation (fr) in a dfxp file.
+
+        """
+        sub_file = os.path.join(self.subs_data_dir, 'Timed_text.sv.dfxp')
+        sc = self._upload_and_verify(self.tv, sub_file, 'French', 'fr')
+        self.assertEqual(sc, 72)
+
+    def test_xml_entities(self):
+        """Upload translation with ampersand in text.
+
+        """
+        sub_file = os.path.join(self.subs_data_dir, 'xml_entities.en.srt')
+        sc = self._upload_and_verify(self.tv, sub_file, 'French', 'fr')
+        self.assertEqual(sc, 72)
