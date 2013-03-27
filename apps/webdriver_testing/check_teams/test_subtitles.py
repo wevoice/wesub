eff85ac62eef94b2853d838ea1bdf7297639cd1c
Janet <jed@pculture>
2013-03-27 21:08:09 +0100

    add a test for i2179

--- apps/webdriver_testing/check_teams/test_subtitles.py
+++ apps/webdriver_testing/check_teams/test_subtitles.py
@@ -22,6 +22,7 @@ class TestCaseTeamSubtitles(WebdriverTestCase):
     def setUpClass(cls):
         super(TestCaseTeamSubtitles, cls).setUpClass()
         cls.data_utils = data_helpers.DataHelpers()
+        cls.sub_editor = subtitle_editor.SubtitleEditor(cls)
         cls.menu = unisubs_menu.UnisubsMenu(cls)
         cls.user = UserFactory.create()
         cls.team = TeamMemberFactory.create(team__workflow_enabled=True,
@@ -77,16 +78,10 @@ class TestCaseTeamSubtitles(WebdriverTestCase):
             video=video, 
             added_by=self.user)
 
-        self.video_language_pg.open_video_lang_page(video.video_id, 'sv')
-        self.assertIn('from English', self.video_language_pg.view_notice())
         self.video_language_pg.log_in(self.user.username, 'password')
         self.video_language_pg.open_video_lang_page(video.video_id, 'en')
         self.video_language_pg.unpublish(delete=True)
 
-        #sl = video.subtitle_language('en')
-        #sl.unpublish(delete=True)
-        #sl.save()
-        #self.video_language_pg.open_video_lang_page(video.video_id, 'sv')
         sl_sv = video.subtitle_language('sv')
         sub_file = os.path.join(self.subs_data_dir, 'srt-full.srt')
         video = VideoUrlFactory().video
@@ -100,6 +95,42 @@ class TestCaseTeamSubtitles(WebdriverTestCase):
         self.data_utils.upload_subs(video, orig_data)
         self.logger.info('SV IS_FORKED %s' % sl_sv.is_forked)
         self.assertTrue(sl_sv.is_forked)
+
+    def test_delete_source_edit_forked_translation(self):
+        """Deleting source forks translations and new source uploads not blocked.
+
+        """
+        sub_file = os.path.join(self.subs_data_dir, 'Timed_text.en.srt')
+        video = VideoUrlFactory().video
+        orig_data = {'language_code': 'en',
+                     'video': video.pk,
+                     'primary_audio_language_code': 'en',
+                     'draft': open(sub_file),
+                     'is_complete': True,
+                     'complete': 1,
+                    }
+        self.data_utils.upload_subs(video, orig_data)
+
+        translation = os.path.join(self.subs_data_dir, 'Timed_text.sv.dfxp')
+        trans_data = {'language_code': 'sv',
+                      'video': video.pk,
+                      'from_language_code': 'en',
+                      'draft': open(translation),
+                     }
+        self.data_utils.upload_subs(video, trans_data)
+        TeamVideoFactory.create(
+            team=self.team, 
+            video=video, 
+            added_by=self.user)
+        self.video_language_pg.open_video_lang_page(video.video_id, 'en')
+
+        self.video_language_pg.log_in(self.user.username, 'password')
+        self.video_language_pg.open_video_lang_page(video.video_id, 'en')
+        self.video_language_pg.unpublish(delete=True)
+        self.video_language_pg.open_video_lang_page(video.video_id, 'sv')
+        self.video_language_pg.edit_subtitles()
+        self.assertEqual('Typing', self.sub_editor.dialog_title())
+
 
 
 
