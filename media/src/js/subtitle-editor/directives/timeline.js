cc7e08d9841b1f964aee9f3965f578ba0fa23402
Ben Dean-Kawamura <ben@pculture.org>
2013-05-27 11:13:53 -0400

    Refactored timeline-timing directive.
    Moved code inside the link() function.  Made a couple small changes to
    simplify the code.

--- media/src/js/subtitle-editor/directives/timeline.js
+++ media/src/js/subtitle-editor/directives/timeline.js
@@ -39,76 +39,76 @@ var angular = angular || null;
     }
 
     directives.directive('timelineTiming', function() {
-        var width=0, height=65; // dimensions of the canvas
-        var view = null;
+        return function link(scope, elem, attrs) {
+            var width=0, height=65; // dimensions of the canvas
+            var view = null;
+            var canvas = $(elem);
+            var canvasElt = elem[0];
+            var container = canvas.parent();
 
-        function resizeCanvas(elem) {
-            // Resize the canvas so that it's width matches the
-            // width of its container, but also make sure that it's a whole
-            // number of pixels.
-            var canvas = elem[0];
-            width = canvas.width = Math.floor($(elem).parent().width());
-            $(elem).css('width', width + 'px');
-        }
-        function trackWindowResize(scope, elem) {
-            $(window).resize(function() {
-                resizeCanvas(elem);
-                drawCanvas(scope, elem);
-            });
-        }
-        function drawSecond(ctx, xPos, t) {
-            // draw the second text on the timeline
-            ctx.fillStyle = '#686868';
-            var metrics = ctx.measureText(t);
-            var x = xPos - (metrics.width / 2);
-            ctx.fillText(t, x, 60);
-        }
-        function drawTics(ctx, xPos) {
-            // draw the tic marks between seconds
-            ctx.strokeStyle = '#686868';
-            var divisions = 4;
-            var step = view.widthPerSecond / divisions;
-            ctx.lineWidth = 1;
-            ctx.beginPath();
-            for(var i = 1; i < divisions; i++) {
-                var x = Math.floor(0.5 + xPos + step * i);
-                x += 0.5;
-                ctx.moveTo(x, 60);
-                if(i == divisions / 2) {
-                    // draw an extra long tic for the 50% mark;
-                    ctx.lineTo(x, 45);
+            function resizeCanvas() {
+                // Resize the canvas so that it's width matches the
+                // width of its container, but also make sure that it's a whole
+                // number of pixels.
+                width = canvasElt.width = Math.floor(container.width());
+                canvas.css('width', width + 'px');
+            }
+            function drawSecond(ctx, xPos, t) {
+                // draw the second text on the timeline
+                ctx.fillStyle = '#686868';
+                var metrics = ctx.measureText(t);
+                var x = xPos - (metrics.width / 2);
+                ctx.fillText(t, x, 60);
+            }
+            function drawTics(ctx, xPos) {
+                // draw the tic marks between seconds
+                ctx.strokeStyle = '#686868';
+                var divisions = 4;
+                var step = view.widthPerSecond / divisions;
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                for(var i = 1; i < divisions; i++) {
+                    var x = Math.floor(0.5 + xPos + step * i);
+                    x += 0.5;
+                    ctx.moveTo(x, 60);
+                    if(i == divisions / 2) {
+                        // draw an extra long tic for the 50% mark;
+                        ctx.lineTo(x, 45);
+                    } else {
+                        ctx.lineTo(x, 50);
+                    }
+                }
+                ctx.stroke();
+            }
+            function drawCanvas() {
+                var ctx = canvasElt.getContext("2d");
+                ctx.clearRect(0, 0, width, height);
+                ctx.font = 'bold ' + (height / 5) + 'px sans';
+
+                var startTime = Math.floor(Math.max(view.startTime / 1000, 0));
+                if(scope.duration !== null) {
+                    var endTime = Math.floor(Math.min(view.endTime / 1000,
+                                scope.duration / 1000));
                 } else {
-                    ctx.lineTo(x, 50);
+                    var endTime = Math.floor(view.endTime / 1000);
+                }
+
+                for(var t = startTime; t < endTime; t++) {
+                    var xPos = (t - (view.startTime / 1000)) * view.widthPerSecond;
+                    drawSecond(ctx, xPos, t);
+                    drawTics(ctx, xPos);
                 }
             }
-            ctx.stroke();
-        }
-        function drawCanvas(scope, elem) {
-            var ctx = elem[0].getContext("2d");
-            ctx.clearRect(0, 0, width, height);
-            ctx.font = 'bold ' + (height / 5) + 'px sans';
 
-            view = calcTimelineView(scope, width);
-            var startTime = Math.floor(Math.max(view.startTime / 1000, 0));
-            if(scope.duration !== null) {
-                var endTime = Math.floor(Math.min(view.endTime / 1000,
-                            scope.duration / 1000));
-            } else {
-                var endTime = Math.floor(view.endTime / 1000);
-            }
-
-            for(var t = startTime; t < endTime; t++) {
-                var xPos = (t - (view.startTime / 1000)) * view.widthPerSecond;
-                drawSecond(ctx, xPos, t);
-                drawTics(ctx, xPos);
-            }
-        }
-        return function link(scope, elem, attrs) {
-            resizeCanvas(elem);
-            trackWindowResize(scope, elem);
+            resizeCanvas();
+            $(window).resize(function() {
+                resizeCanvas();
+                drawCanvas();
+            });
             scope.$watch('currentTime + ":" + duration',
                 function(newValue, oldValue) {
-                    drawCanvas(scope, elem);
+                    view = calcTimelineView(scope, width);
+                    drawCanvas();
             });
         }
     });
