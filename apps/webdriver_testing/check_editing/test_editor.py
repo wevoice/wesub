17c62f943ecc115a4cff552dc690bc86853c5fe6
Janet <jed@pculture>
2013-08-02 16:53:47 +0200

    no wait for title

--- apps/webdriver_testing/check_editing/test_editor.py
+++ apps/webdriver_testing/check_editing/test_editor.py
@@ -50,7 +50,6 @@ class TestCaseLeftSide(WebdriverTestCase):
     def tearDown(self):
         if self.editor_pg.is_element_present(self.editor_pg.EXIT_BUTTON):
             self.editor_pg.exit()
-        self.logger.info(self.video_pg.video_title())
         
     def test_reference_lang__forked(self):
         """Default reference lang for forked translation is the same lang. """
