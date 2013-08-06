b2a558c97c498714a239368e0d99269b23f5c9bb
Ben Dean-Kawamura <ben@pculture.org>
2013-08-06 13:39:07 -0400

    Removed code that updates the search index based on widget views
    I'm not 100% sure, but I believe that this was causing issues on the
    site.  I would observe it running then quickly afterwards we would
    have several different database transactions keeping locks on the DB
    for long periods of time (> 10 minutes).
    
    I think this has to do with the way that the code was structured, we
    would try to index 200 videos at a time which takes quite a while.
    Also since we would split things up into chunks, then send them all to
    celery at once so we would have many workers updating the index at the
    same time.
    
    We don't really need this code because we re-index the videos once a
    day in the search.tasks.update_search_index task.  In the long term,
    I'm hoping to replace that task as well and restore the functionality
    that I'm removing here: videos that get page views get their index
    updated more often.

--- utils/celery_search_index.py
+++ utils/celery_search_index.py
@@ -62,23 +62,6 @@ def update_search_index(model_class, pk):
 
     search_index.update_object(obj)
 
-@task()
-def update_search_index_for_qs(model_class, pks):
-    start = time.time()
-
-    qs = model_class._default_manager.filter(pk__in=pks)
-
-    try:
-        search_index = site.get_index(model_class)
-    except NotRegistered:
-        log(u'Seacrh index is not registered for %s' % model_class)
-        return None
-
-    search_index.backend.update(search_index, qs)
-
-    LogEntry(num=len(pks), time=time.time()-start).save()
-
-
 class LogEntry(rmodels.Model):
     num = rmodels.IntegerField()
     time = rmodels.FloatField()
