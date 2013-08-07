e36c2f77dc75e123802a7d3d6b8e6d4b63c61e99
Evan Hazlett <ejhazlett@gmail.com>
2013-08-06 20:11:28 -0400

    docker updates (build_media)

--- .docker/build_media.sh
+++ .docker/build_media.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-echo "Compiling Media..."
-
-APP_NAME=unisubs
-APP_ROOT=/opt/apps
-APP_DIR=$APP_ROOT/$APP_NAME
-VE_ROOT=/opt/ve
-VE_DIR=$VE_ROOT/$APP_NAME
+source /usr/local/bin/config_env.sh
 
 cd $APP_DIR
+echo "Compiling Media..."
 $VE_DIR/bin/python manage.py compile_media --compilation-level=ADVANCED_OPTIMIZATIONS --settings=unisubs_settings
+echo "Uploading to S3..."
 $VE_DIR/bin/python manage.py send_to_s3 --settings=unisubs_settings
