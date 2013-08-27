74334ceb0aa1781ee008eb08177583091ffe7679
Evan Hazlett <ejhazlett@gmail.com>
2013-08-27 13:41:24 -0400

    added docker support for newrelic (worker)

--- .docker/worker.sh
+++ .docker/worker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source /usr/local/bin/config_env
 
+PRE=""
+CMD="$VE_DIR/bin/python manage.py celeryd -E $CELERY_OPTS --scheduler=djcelery.schedulers.DatabaseScheduler --settings=unisubs_settings"
+
 echo "$MAILNAME" > /etc/mailname
 
 cat << EOF > /etc/postfix/main.cf
@@ -33,5 +36,10 @@ EOF
 /etc/init.d/postfix start
 
 cd $APP_DIR
+if [ ! -z "$NEW_RELIC_LICENSE_KEY" ] ; then
+    $VE_DIR/bin/pip install -U newrelic
+    PRE="$VE_DIR/bin/newrelic-admin run-program "
+fi
+
 echo "Starting Worker..."
-$VE_DIR/bin/python manage.py celeryd -E $* --scheduler=djcelery.schedulers.DatabaseScheduler --settings=unisubs_settings
+$PRE $CMD
