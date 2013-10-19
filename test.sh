9fa8855d27f67785943585b4ef63f73eead25995
Evan Hazlett <ejhazlett@gmail.com>
2013-10-18 20:47:19 -0400

    updated jenkins test script

--- test.sh
+++ test.sh
@@ -30,7 +30,7 @@ RABBITMQ_PORT=$($DOCKER port $RABBITMQ 5672)
 echo "Containers: Solr:$SOLR:$SOLR_PORT RabbitMQ:$RABBITMQ:$RABBITMQ_PORT"
 
 # run tests
-AMARA_APP=$($DOCKER run -h unisubs.example.com -e SKIP_CODE_PULL=true -e TEST_IPADDR=$IPADDR -e TEST_BROKER_PORT=$RABBITMQ_PORT -e TEST_SOLR_PORT=$SOLR_PORT amara-app /usr/local/bin/test_app)
+AMARA_APP=$($DOCKER run -i -h unisubs.example.com -e SKIP_CODE_PULL=true -e TEST_IPADDR=$IPADDR -e TEST_BROKER_PORT=$RABBITMQ_PORT -e TEST_SOLR_PORT=$SOLR_PORT amara-app /usr/local/bin/test_app)
 
 sleep 5
 
