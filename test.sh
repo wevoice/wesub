bcf1a71b8fb8b3e7295aa5afdad5af1a978f3353
Evan Hazlett <ejhazlett@gmail.com>
2013-10-18 20:27:30 -0400

    updated jenkins test script

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+DOCKER_HOST=${DOCKER_HOST:-}
 IPADDR=`ifconfig docker0 | awk '/inet addr/ {split ($2,A,":"); print A[2]}'`
+if [ ! -z "$DOCKER_HOST" ] ; then
+    DOCKER="docker -H $DOCKER_HOST"
+else
+    DOCKER="docker"
+fi
 ROOT=`pwd`
 for IMG in amara-app amara-mysql amara-solr amara-memcached amara-rabbitmq
 do
@@ -15,20 +21,20 @@ do
     fi
 done
 
-SOLR=$(docker run -i -t -d amara-solr)
-SOLR_PORT=$(docker port $SOLR 8983)
-RABBITMQ=$(docker run -i -t -d amara-rabbitmq)
-RABBITMQ_PORT=$(docker port $RABBITMQ 5672)
+SOLR=$($DOCKER run -i -t -d amara-solr)
+SOLR_PORT=$($DOCKER port $SOLR 8983)
+RABBITMQ=$($DOCKER run -i -t -d amara-rabbitmq)
+RABBITMQ_PORT=$($DOCKER port $RABBITMQ 5672)
 
 echo "Containers: Solr:$SOLR:$SOLR_PORT RabbitMQ:$RABBITMQ:$RABBITMQ_PORT"
 
 # run tests
-docker run -i -t -h unisubs.example.com -e SKIP_CODE_PULL=true -e TEST_IPADDR=$IPADDR -e TEST_BROKER_PORT=$RABBITMQ_PORT -e TEST_SOLR_PORT=$SOLR_PORT amara-app /usr/local/bin/test_app
+$DOCKER run -i -t -h unisubs.example.com -e SKIP_CODE_PULL=true -e TEST_IPADDR=$IPADDR -e TEST_BROKER_PORT=$RABBITMQ_PORT -e TEST_SOLR_PORT=$SOLR_PORT amara-app /usr/local/bin/test_app
 
 sleep 5
 
 for CNT in $SOLR $RABBITMQ
 do
-    docker kill $CNT
-    docker rm $CNT
+    $DOCKER kill $CNT
+    $DOCKER rm $CNT
 done
