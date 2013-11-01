bcdf326b872d895d00528a470b23054a35213d98
Ben Dean-Kawamura <ben@pculture.org>
2013-11-01 12:09:59 -0400

    Step using eventlet celery pool.
    Now that we fixed billiard to version 2.7, we can go back to
    processes.

--- .docker/worker.sh
+++ .docker/worker.sh
@@ -2,7 +2,7 @@
 source /usr/local/bin/config_env
 
 PRE=""
-CMD="$VE_DIR/bin/python manage.py celery worker --scheduler=djcelery.schedulers.DatabaseScheduler --loglevel=DEBUG -B -E -Peventlet $CELERY_OPTS --settings=unisubs_settings"
+CMD="$VE_DIR/bin/python manage.py celery worker --scheduler=djcelery.schedulers.DatabaseScheduler --loglevel=DEBUG -B -E $CELERY_OPTS --settings=unisubs_settings"
 
 cd $APP_DIR
 if [ ! -z "$NEW_RELIC_LICENSE_KEY" ] ; then
