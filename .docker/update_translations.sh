36ded39b1ac1ab0e5ec467de2b06be8b6d2b730f
Evan Hazlett <ejhazlett@gmail.com>
2013-11-25 11:00:17 -1000

    use latest transifex client

--- .docker/update_translations.sh
+++ .docker/update_translations.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source /usr/local/bin/config_env
-
+# install client
+pip install transifex-client
+# update
 cd $APP_DIR
 # get private key from s3 to clone private repo
 s3cmd -c /etc/s3cfg get --force s3://amara/admin/keys/amara-transifex /root/.ssh/amara_transifex
