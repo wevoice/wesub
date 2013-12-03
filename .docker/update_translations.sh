4dafb88d4b8b3ce00a242bd49cb2ecedff4da89f
Evan Hazlett <ejhazlett@gmail.com>
2013-12-03 01:08:43 -0500

    more transifex updates

--- .docker/update_translations.sh
+++ .docker/update_translations.sh
@@ -4,13 +4,13 @@ source /usr/local/bin/config_env
 cd $APP_DIR
 # get private key from s3 to clone private repo
 s3cmd -c /etc/s3cfg get --force s3://amara/admin/keys/amara-transifex /root/.ssh/amara_transifex
-chmod 600 /.ssh/amara_transifex
-cat << EOF > /.ssh/config
+chmod 600 /root/.ssh/amara_transifex
+cat << EOF > /root/.ssh/config
 Host github.com
     IdentityFile /root/.ssh/amara_transifex
 EOF
 
-cat << EOF > /.gitconfig
+cat << EOF > /root/.gitconfig
 [user]
     name = transifex
     email = admin@amara.org
