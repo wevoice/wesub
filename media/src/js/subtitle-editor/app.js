64710e8f8133ba77593cd6f0d2b1a4eb27683944
Sylvain Chevalier <sylvain@pculture.org>
2013-12-06 20:31:53 +0800

    fix bug with function to determine if copy timings should be available

--- media/src/js/subtitle-editor/app.js
+++ media/src/js/subtitle-editor/app.js
@@ -109,8 +109,8 @@ var angular = angular || null;
             $scope.$root.$emit('work-done');
 	}
         $scope.copyTimingEnabled = function() {
-            return (workingSubtitles.subtitleList.length() > 0 &&
-                     referenceSubtitles.subtitleList.syncedCount > 0)
+            return ($scope.workingSubtitles.subtitleList.length() > 0 &&
+                     $scope.referenceSubtitles.subtitleList.syncedCount > 0)
         }
         $scope.timeline = {
             shownSubtitle: null,
