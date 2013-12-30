73df211586880967ca49c916f3b09257a7577008
Evan Hazlett <ejhazlett@gmail.com>
2013-12-30 15:39:20 -0500

    update test for jenkins

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-AWS_ID=${AWS_ACCESS_ID:-}
-AWS_KEY=${AWS_SECRET_KEY:-}
+DOCKER_HOST="$1"
+AWS_ID=${2:-}
+AWS_KEY=${3:-}
 REV=${REVISION:-staging}
 SETTINGS_REV=${SETTINGS_REVISION:-$REV}
-RUN_SELENIUM=${RUN_SELENIUM:-$2}
-DOCKER_HOST="$1"
+RUN_SELENIUM=${4:-}
 if [ ! -z "$DOCKER_HOST" ] ; then
     DOCKER="docker -H $DOCKER_HOST"
     IPADDR="$DOCKER_HOST"
@@ -12,6 +12,12 @@ else
     DOCKER="docker"
     IPADDR=`ifconfig docker0 | awk '/inet addr/ {split ($2,A,":"); print A[2]}'`
 fi
+echo "Environment"
+echo "  Host: $DOCKER_HOST"
+echo "  AWS_ID: $AWS_ID"
+echo "  AWS_KEY: $AWS_KEY"
+echo "  Revision: $REVISION"
+echo "  Selenium: $RUN_SELENIUM"
 ROOT=`pwd`
 for IMG in amara-app amara-solr amara-rabbitmq amara-memcached
 do
