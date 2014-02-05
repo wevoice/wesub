7d59b30554e5dbfbdef4846025074eb68ec1bfbf
Evan Hazlett <ejhazlett@gmail.com>
2014-02-05 02:07:55 -0500

    fixes for test script

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 DOCKER_HOST=${1:-}
 AWS_ID=${2:-}
 AWS_KEY=${3:-}
@@ -7,7 +8,8 @@ SETTINGS_REV=${SETTINGS_REVISION:-$REV}
 RUN_SELENIUM=${4:-}
 if [ ! -z "$DOCKER_HOST" ] ; then
     DOCKER="docker -H $DOCKER_HOST"
-    IPADDR="$DOCKER_HOST"
+    #IPADDR="$DOCKER_HOST"
+    IPADDR=`echo ${DOCKER_HOST} | awk -F  ":" '{print $1}'`
 else
     DOCKER="docker"
     IPADDR=`ifconfig docker0 | awk '/inet addr/ {split ($2,A,":"); print A[2]}'`
@@ -29,6 +31,7 @@ do
             cd $ROOT && $DOCKER build -t $IMG .
         else
             if [ ! -e "$ROOT/.docker/$IMG" ] ; then
+                cd $ROOT
                 git clone https://github.com/pculture/docker-$IMG $ROOT/.docker/$IMG
             fi
             cd $ROOT/.docker/$IMG && $DOCKER build -t $IMG .
@@ -48,9 +51,9 @@ MEMCACHED_PORT=$($DOCKER port $MEMCACHED 11211 | awk -F: '{ print $2; }')
 echo "Containers: Solr:$SOLR:$SOLR_PORT RabbitMQ:$RABBITMQ:$RABBITMQ_PORT Memcached:$MEMCACHED:$MEMCACHED_PORT"
 
 # run tests
-TEST_APP=$($DOCKER run -i -t -h unisubs.example.com -e AWS_ACCESS_ID=$AWS_ID -e AWS_SECRET_KEY=$AWS_KEY -e REVISION=$REV -e TEST_IPADDR=$IPADDR -e TEST_BROKER_PORT=$RABBITMQ_PORT -e TEST_SOLR_PORT=$SOLR_PORT -e TEST_MEMCACHED_PORT=$MEMCACHED_PORT -e DJANGO_LIVE_TEST_SERVER_ADDRESS='localhost:8090-8100,9000-9200' -e RUN_SELENIUM="$RUN_SELENIUM" amara-app /usr/local/bin/test_app)
+TEST_APP=$($DOCKER run -i -d -h unisubs.example.com -e AWS_ACCESS_ID=$AWS_ID -e AWS_SECRET_KEY=$AWS_KEY -e REVISION=$REV -e TEST_IPADDR=$IPADDR -e TEST_BROKER_PORT=$RABBITMQ_PORT -e TEST_SOLR_PORT=$SOLR_PORT -e TEST_MEMCACHED_PORT=$MEMCACHED_PORT -e DJANGO_LIVE_TEST_SERVER_ADDRESS='localhost:8090-8100,9000-9200' -e RUN_SELENIUM="$RUN_SELENIUM" amara-app /usr/local/bin/test_app)
 echo "Test Container: $TEST_APP"
-$DOCKER wait $TEST_APP
+RETVAL=$($DOCKER wait $TEST_APP)
 #$DOCKER run -i -t -h unisubs.example.com -e AWS_ACCESS_ID=$AWS_ID -e AWS_SECRET_KEY=$AWS_KEY -e REVISION=$REV -e TEST_IPADDR=$IPADDR -e TEST_BROKER_PORT=$RABBITMQ_PORT -e TEST_SOLR_PORT=$SOLR_PORT -e TEST_MEMCACHED_PORT=$MEMCACHED_PORT -e DJANGO_LIVE_TEST_SERVER_ADDRESS="localhost:8090-8100,9000-9200" -e RUN_SELENIUM="$RUN_SELENIUM" amara-app bash
 
 RETVAL=$?
