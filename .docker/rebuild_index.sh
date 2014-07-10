52d10080efade33ab7ce6818b630c600db2ef199
Evan Hazlett <ejhazlett@gmail.com>
2014-07-10 02:18:01 -0400

    updated rebuild index script

--- .docker/rebuild_index.sh
+++ .docker/rebuild_index.sh
@@ -3,4 +3,4 @@ source /usr/local/bin/config_env
 
 cd $APP_DIR
 echo "Rebuilding index..."
-$VE_DIR/bin/python manage.py rebuild_index_ordered --settings=unisubs_settings
+$VE_DIR/bin/python manage.py rebuild_index_ordered --noinput --settings=unisubs_settings
