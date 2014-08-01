9ab80d756a54853e1bb4c57a767b4935d83783e1
Ben Dean-Kawamura <ben@pculture.org>
2014-08-01 15:56:37 -0400

    Added nicer way to run dev commands
    Made a new directory that contains commands to do common tasks with
    the dev environment.
    
    The system works similarly to virtualenv.  You source the activate
    script, which adds the directory to your path.  When you're done you
    run deactivate.
    
    Features include:
      - You can run the commands from any directory
      - Less typing
      - Quick way to start up a bash prompt
    
    Also removed a couple scripts that had to do with vagrant.  I don't
    think we plan on supporting that stuff anymore.

--- bin/dev_syncdb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-fig run --rm app python manage.py syncdb --all --noinput
-fig run --rm app python manage.py migrate --fake
--- bin/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-fig run --rm app python manage.py test --settings=dev_settings_test "$@"
