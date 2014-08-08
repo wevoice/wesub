34c473bd8e882eb0198ceb707053de307b132dd9
Ben Dean-Kawamura <ben@pculture.org>
2014-08-08 13:59:33 -0400

    Remove references to STATIC_URL_BASE (#1632)
    We weren't even really using the value.

--- apps/videos/share_utils.py
+++ apps/videos/share_utils.py
@@ -32,13 +32,6 @@ def _add_share_panel_context(context, facebook_url, twitter_url, embed_params,
     
     context["share_panel_facebook_url"] = facebook_url
     context["share_panel_twitter_url"] = twitter_url
-    
-    ec_context = {
-        'embed_params': json(embed_params),
-        'STATIC_URL': settings.STATIC_URL,
-        'STATIC_URL_BASE': settings.STATIC_URL_BASE,
-    }
-    
     context["share_panel_email_url"] = email_url
     context["share_panel_permalink"] = permalink
 
