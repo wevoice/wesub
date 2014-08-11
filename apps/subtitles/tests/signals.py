07377c73d4529739e7d9e846eeb696cd5c1e72ef
Ben Dean-Kawamura <ben@pculture.org>
2014-08-11 13:03:55 -0400

    Honor send_subtitles_changed when saving SubtitleLanguages
    Oops, not sure how I missed this one originally.

--- apps/subtitles/tests/signals.py
+++ apps/subtitles/tests/signals.py
@@ -76,3 +76,12 @@ class SignalsTest(TestCase):
         self.assertEquals(self.subtitles_changed_handler.call_count, 1)
         self.subtitles_changed_handler.assert_called_with(
             signal=mock.ANY, sender=language)
+
+    def test_send_subtitles_changed_false(self):
+        v1 = pipeline.add_subtitles(self.video, 'en', None)
+        language = v1.subtitle_language
+        self.subtitles_changed_handler.reset_mock()
+
+        language.subtitles_complete = True
+        language.save(send_subtitles_changed=False)
+        self.assertEquals(self.subtitles_changed_handler.call_count, 0)
