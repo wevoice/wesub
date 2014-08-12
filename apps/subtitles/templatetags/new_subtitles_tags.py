23886cc29c0ba63ed54d362feb39ffe7f2acc160
Sylvain Chevalier <sylvain@pculture.org>
2014-08-12 16:14:44 +0200

    Removed unused template tag

--- apps/subtitles/templatetags/new_subtitles_tags.py
+++ apps/subtitles/templatetags/new_subtitles_tags.py
@@ -28,24 +28,6 @@ from subtitles.forms import SubtitlesUploadForm
 
 register = template.Library()
 
-@register.inclusion_tag('subtitles/_upload_subtitles.html', takes_context=True)
-def upload_subtitles(context, video):
-    context['video'] = video
-    initial = {}
-
-    current_language = context.get('language')
-    if current_language:
-        initial['language_code'] = current_language.language_code
-
-    if video.primary_audio_language_code:
-        initial['primary_audio_language_code'] = video.primary_audio_language_code
-
-
-    context['form'] = SubtitlesUploadForm(context['user'], video,
-                                          initial=initial)
-
-    return context
-
 @register.filter
 def visibility_display(subtitle_version):
     '''
