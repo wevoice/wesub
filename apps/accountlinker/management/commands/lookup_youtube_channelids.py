388d1b8495e82810414dd3c5a481a3685242f297
Ben Dean-Kawamura <ben@pculture.org>
2014-08-13 14:31:55 -0400

    Added extra option for lookup_youtube_channelids
    This will let us lookup channel ids for new accounts right after we
    take down the site in preparation for the migration.

--- apps/accountlinker/management/commands/lookup_youtube_channelids.py
+++ apps/accountlinker/management/commands/lookup_youtube_channelids.py
@@ -16,9 +16,10 @@
 # along with this program.  If not, see
 # http://www.gnu.org/licenses/agpl-3.0.html.
 
+from optparse import make_option
+
 from django.core.management.base import BaseCommand
 from django.db import connection
-
 # hack to avoid exceptions to do circular imports
 from haystack import site
 
@@ -26,19 +27,34 @@ from accountlinker.models import ThirdPartyAccount
 from accountlinker import newyoutube
 
 class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
+    option_list = BaseCommand.option_list + (
+        make_option('--after-id', dest='after_id', default=None,
+                    metavar='ID', help='Only lookup accounts later than ID'),
+    )
+
+    def handle(self, *args, **options):
+        self.options = options
         self.find_account_ids_to_update()
         while self.account_ids_to_update:
-            print '* %s accounts to update' % len(self.account_ids_to_update)
+            self.stdout.write('* %s accounts to update\n' %
+                              len(self.account_ids_to_update))
             self.update_one_account()
             # commit our transaction to try to avoid any deadlocks
             connection.cursor().execute("COMMIT")
+        self.stdout.write('lookup complete\n')
+        self.stdout.write('to lookup new accounts use:\n')
+        self.stdout.write('lookup_youtube_channelids --after-id %s\n' %
+                          self.max_account_id)
 
     def find_account_ids_to_update(self):
-        self.account_ids_to_update = list(
-            ThirdPartyAccount.objects
-            .filter(channel_id='')
-            .values_list('id', flat=True))
+        qs = ThirdPartyAccount.objects.filter(channel_id='')
+        if self.options['after_id']:
+            qs = qs.filter(id__gt=self.options['after_id'])
+        self.account_ids_to_update = list(qs.values_list('id', flat=True))
+        if len(self.account_ids_to_update) > 0:
+            self.max_account_id = max(self.account_ids_to_update)
+        else:
+            self.max_account_id = 0
 
     def update_one_account(self):
         account_id = self.account_ids_to_update.pop()
@@ -46,7 +62,7 @@ class Command(BaseCommand):
         try:
             channel_id = self.lookup_channel_id(account)
         except StandardError, e:
-            print 'error with account %s (%s)' % (account, e)
+            self.stdout.write('error with account %s (%s)\n' % (account, e))
             return
         account.channel_id = channel_id
         account.save()
