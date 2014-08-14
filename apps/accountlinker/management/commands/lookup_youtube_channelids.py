135e9007895253d635284fe4bdc4dfed217902c7
Ben Dean-Kawamura <ben@pculture.org>
2014-08-14 16:31:36 -0400

    Added extra printout after lookup_youtube_channelids command
    There are a couple accounts with different usernames, but the same
    channel id.  We're going to drop some of them.  This printout will
    help us to know that we're not going to drop ones that we don't
    expect.

--- apps/accountlinker/management/commands/lookup_youtube_channelids.py
+++ apps/accountlinker/management/commands/lookup_youtube_channelids.py
@@ -20,6 +20,7 @@ from optparse import make_option
 
 from django.core.management.base import BaseCommand
 from django.db import connection
+from django.db.models import Count
 # hack to avoid exceptions to do circular imports
 from haystack import site
 
@@ -41,10 +42,61 @@ class Command(BaseCommand):
             self.update_one_account()
             # commit our transaction to try to avoid any deadlocks
             connection.cursor().execute("COMMIT")
+        self.print_exit_message()
+
+    def print_exit_message(self):
         self.stdout.write('lookup complete\n')
         self.stdout.write('to lookup new accounts use:\n')
         self.stdout.write('lookup_youtube_channelids --after-id %s\n' %
                           self.max_account_id)
+        dups = self.find_dup_channel_ids()
+        if not dups:
+            return
+        self.stdout.write("\nThere are some accounts with duplicate channel "
+                          "ids.  Only the first created account will be "
+                          "migrated\n")
+        for first, extra in dups:
+            self.stdout.write("Youtube user %s:\n" % first.username)
+            self.stdout.write("  will keep %s\n" % self.tpa_display_string(first))
+            for account in extra:
+                self.stdout.write("  will drop %s\n" %
+                                  self.tpa_display_string(account))
+            self.stdout.write("\n")
+
+    def tpa_display_string(self, account):
+        if account.is_team_account:
+            teams = list(account.teams.all())
+            if len(teams) > 1:
+                return "account for teams %s" % (teams,)
+            else:
+                return "account for team %s" % (teams[0])
+        else:
+            users = list(account.users.all())
+            if len(users) > 1:
+                return "account for users %s" % (users,)
+            else:
+                return "account for user %s" % (users[0])
+
+    def find_dup_channel_ids(self):
+        dups = []
+        current_channel_id = None
+        dup_channel_ids = list(
+            ThirdPartyAccount.objects
+            .values_list('channel_id', flat=True)
+            .exclude(channel_id='')
+            .annotate(count=Count('channel_id'))
+            .filter(count__gt=1)
+        )
+        qs = (ThirdPartyAccount.objects.
+              filter(channel_id__in=dup_channel_ids)
+              .order_by('id'))
+        for account in qs:
+            if account.channel_id != current_channel_id:
+                dups.append((account, []))
+                current_channel_id = account.channel_id
+            else:
+                dups[-1][1].append(account)
+        return dups
 
     def find_account_ids_to_update(self):
         qs = ThirdPartyAccount.objects.filter(channel_id='')
