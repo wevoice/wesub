21eb00f85a402c3fbf00be28e07ec8c97e0cdff4
Ben Dean-Kawamura <ben@pculture.org>
2014-09-19 11:15:25 -0400

    Support browsing the django-rest-framework
    Added some code to make our staticmedia app work like the normal
    django staticfiles one.
    
    Added basic template for rest_framework pages.

--- apps/staticmedia/urls.py
+++ apps/staticmedia/urls.py
@@ -42,7 +42,10 @@ else:
         '/flowplayer/', document_root=os.path.join(settings.STATIC_ROOT, 'flowplayer')
     ) + static(
         '/fonts/', document_root=os.path.join(settings.STATIC_ROOT, 'fonts')
-    ) + static('/admin/', document_root=utils.admin_media_root())
+    )
+
+    for static_dir in utils.app_static_media_dirs():
+        urlpatterns += static('/', document_root=static_dir)
 
 if settings.DEBUG:
     urlpatterns += patterns(
