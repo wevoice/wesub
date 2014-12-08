7e751170026249b7f889b593193b0b352aee3ea1
Sylvain Chevalier <sylvain@pculture.org>
2014-12-08 22:58:00 +0100

    Tentative fix for mouse over on Chrome in statistics

--- utils/graphing.py
+++ utils/graphing.py
@@ -1,17 +1,18 @@
 import pygal
 from pygal.style import LightGreenStyle
+import base64
 import logging
 
 logger = logging.getLogger("Graphs")
 
 def plot(data, title=None, graph_type='Pie'):
     if graph_type == 'Pie':
-        pie_chart = pygal.Pie(disable_xml_declaration=True, style=LightGreenStyle)
+        pie_chart = pygal.Pie(style=LightGreenStyle)
     else:
-        pie_chart = pygal.HorizontalBar(disable_xml_declaration=True, style=LightGreenStyle)
+        pie_chart = pygal.HorizontalBar(style=LightGreenStyle)
     if title:
         pie_chart.title = title
     data.sort(reverse=True, key=lambda x:x[1])
     for item in data:
         pie_chart.add(item[0], [{'value': item[1], 'label': item[0]}])
-    return pie_chart.render()
+    return base64.b64encode(pie_chart.render())
