7f1fde5fb1135bcfdd0440488fbe67f0484f85ef
jdragojevic <jed@pculture.org>
2015-01-19 09:27:54 +0100

    add login for create page tests

--- apps/webdriver_testing/check_create_page.py
+++ apps/webdriver_testing/check_create_page.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
-# Amara, universalsubtitles.org
-#
-# Copyright (C) 2013 Participatory Culture Foundation
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see
-# http://www.gnu.org/licenses/agpl-3.0.html.
 
+from utils.factories import *
 from webdriver_testing.pages.site_pages import create_page
 from webdriver_testing.pages.site_pages import watch_page
 from webdriver_testing.webdriver_base import WebdriverTestCase
@@ -39,7 +23,7 @@ class TestCaseCreateVideos(WebdriverTestCase):
         user = UserFactory()
         cls.create_pg.log_in(user.username, 'password')    
 
-    def tearDown(self):
+    def setUp(self):
         self.create_pg.open_create_page()
 
     def test_create_youtube(self):
