923b92eeb2a2c7b63848a6a58259e5f07393778f
Ben Dean-Kawamura <ben@pculture.org>
2015-03-26 11:29:09 -0400

    Implemented find_unique_username in the user API
    Reworked the user serializer classes.  Split up the serializer classes
    to create/update users.  They are different enough now that this seems
    simpler.  Removed the functionality to create a login token when doing
    a PUT against an existing user.  This matches how things worked with
    the old API.

--- apps/api/tests/test_users.py
+++ apps/api/tests/test_users.py
@@ -118,6 +118,21 @@ class UserAPITest(TestCase):
             'homepage': 'http://example.com/test/'
         })
 
+    def test_create_user_with_unique_username(self):
+        UserFactory(username='test-user')
+        user, response = self.check_post({
+            'username': 'test-user',
+            'find_unique_username': 1,
+            'email': 'test@example.com',
+            'password': 'test-password',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'full_name': 'Test User',
+            'bio': 'test-bio',
+            'homepage': 'http://example.com/test/'
+        })
+        assert_equal(user.username, 'test-user00')
+
     def test_create_user_partial_data(self):
         self.check_post({
             'username': 'test-user',
@@ -134,6 +149,14 @@ class UserAPITest(TestCase):
             'bio': '',
             'homepage': '',
         })
+
+    def test_create_user_non_unique_username(self):
+        UserFactory(username='test-user')
+        response = self.client.post(self.list_url, {
+            'username': 'test-user',
+            'email': 'test@example.com',
+        })
+        assert_equal(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_user_non_ascii_username(self):
         response = self.client.post(self.list_url, {
@@ -181,7 +204,8 @@ class UserAPITest(TestCase):
         response = self.client.put(self.detail_url(other_user), data={
             'first_name': 'New',
         })
-        assert_equal(response.status_code, status.HTTP_403_FORBIDDEN)
+        assert_equal(response.status_code, status.HTTP_403_FORBIDDEN,
+                     response.content)
 
     def test_cant_change_username(self):
         orig_username = self.user.username
