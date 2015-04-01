35d54efcec5d040b596c52837ea56111b881c4b2
Ben Dean-Kawamura <ben@pculture.org>
2015-04-01 09:14:11 -0400

    Revert "Use youtube language codes as-is (#452)"
    This reverts commit 27ad96fda5168bd8cf18bf4bf33e8cc1bbbc0eef.

--- apps/externalsites/subfetch.py
+++ apps/externalsites/subfetch.py
@@ -46,9 +46,10 @@ def fetch_subs_youtube(video_url):
         video_url.video.newsubtitlelanguage_set.having_versions()
     )
 
-    for language_code in google.get_subtitled_languages(video_id):
+    for bcp47_lc in google.get_subtitled_languages(video_id):
+        language_code = LanguageCode(bcp47_lc, 'bcp47').encode('unisubs')
         if language_code not in existing_langs:
-            subs = google.get_subtitles(video_id, language_code)
-            pipeline.add_subtitles(video_url.video, language_code.lower(), subs,
+            subs = google.get_subtitles(video_id, bcp47_lc)
+            pipeline.add_subtitles(video_url.video, language_code, subs,
                                    note="From youtube", complete=True,
                                    origin=ORIGIN_IMPORTED)
