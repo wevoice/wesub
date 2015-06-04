9d1136a95ad9a4faa9bf9d4dbd759168adc47b68
Sylvain Chevalier <sylvain@pculture.org>
2015-06-04 12:18:57 +0200

    HTML5 videos can be converted to mono audio

--- apps/videos/types/htmlfive.py
+++ apps/videos/types/htmlfive.py
@@ -1,6 +1,6 @@
 # Amara, universalsubtitles.org
 # 
-# Copyright (C) 2013 Participatory Culture Foundation
+# Copyright (C) 2013-2015 Participatory Culture Foundation
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -17,6 +17,9 @@
 # http://www.gnu.org/licenses/agpl-3.0.html.
 
 from videos.types.base import VideoType
+import subprocess, sys, uuid
+import logging
+logger = logging.getLogger("HTML5 type")
 
 class HtmlFiveVideoType(VideoType):
     abbreviation = 'H'
@@ -24,6 +27,25 @@ class HtmlFiveVideoType(VideoType):
 
     valid_extensions = set(['ogv', 'ogg', 'mp4', 'm4v', 'webm'])
 
+    def __init__(self, url):
+        self.url = url
+
     @classmethod
     def matches_video_url(cls, url):
         return cls.url_extension(url) in cls.valid_extensions
+
+    def get_audio_file(self):
+        # File is read from its URL, then converted to mono, in was
+        # so that we do not lose quality with another encoding
+        # TODO: find some way to protect ourselves from huge files
+        output = "/tmp/" + str(uuid.uuid4()) + ".wav"
+        cmd = """avconv -i "{}" -ar 16000 -ac 1 {}""".format(self.url, output)
+        try:
+            subprocess.check_call(cmd, shell=True)
+        except subprocess.CalledProcessError as e:
+            logger.error("CalledProcessError error({}) when running command {}".format(e.returncode, cmd))
+            return None
+        except:
+            logger.error("Unexpected error({}) when running command {}".format(sys.exc_info()[0], cmd))
+            return None
+        return output
