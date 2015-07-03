3d9e27d5eb5f8eff03ea3e90e0f273aeddbd4c42
Ben Dean-Kawamura <ben@pculture.org>
2015-07-03 13:37:22 -0400

    API unit tests fixes

--- apps/api/tests/test_videos.py
+++ apps/api/tests/test_videos.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html.
 
+from __future__ import absolute_import
+
 from django.test import TestCase
 from nose.tools import *
 from rest_framework import status
