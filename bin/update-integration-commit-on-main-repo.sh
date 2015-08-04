b8dcc7d3c37d97f144929b21c75cbdb20f872d28
Ben Dean-Kawamura <ben@pculture.org>
2015-08-04 10:09:18 -0400

    Deleting a bunch of old bin/ scripts

--- bin/update-integration-commit-on-main-repo.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd unisubs-integration &&
-git rev-parse HEAD > ../optional/unisubs-integration
-cd ..
-git add optional/unisubs-integration
-git commit -m "Updated integration repo"
