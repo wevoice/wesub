ac971e0b5a5b2c97211f8272ef05db2ac3e33e16
Ben Dean-Kawamura <ben@pculture.org>
2015-12-11 11:26:43 -0500

    Use host names rather than env variables
    I think this is the new recommended way to do it with docker-compose.
    It's a lot cleaner and I think it fixes some issues if the address
    changes between runs.

--- dev_settings.py
+++ dev_settings.py
@@ -29,10 +29,10 @@ INSTALLED_APPS += (
 )
 
 BROKER_BACKEND = 'amqplib'
-BROKER_HOST = os.environ.get('QUEUE_1_PORT_5672_TCP_ADDR')
+BROKER_HOST = 'queue'
 BROKER_USER = 'guest'
 BROKER_PASSWORD = 'guest'
-BROKER_PORT = os.environ.get('QUEUE_1_PORT_5672_TCP_PORT')
+BROKER_PORT = 5672
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 JS_USE_COMPILED = True
@@ -46,16 +46,15 @@ DATABASES = {
         'NAME': "amara",
         'USER': "amara",
         'PASSWORD': "amara",
-        'HOST': os.environ.get('DB_1_PORT_3306_TCP_ADDR'),
-        'PORT': os.environ.get('DB_1_PORT_3306_TCP_PORT'),
+        'HOST': 'db',
+        'PORT': 3306
         }
     }
 
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '{}:{}'.format(os.environ.get('CACHE_1_PORT_11211_TCP_ADDR'),
-            os.environ.get('CACHE_1_PORT_11211_TCP_PORT')),
+        'LOCATION': 'cache:11211',
     }
 }
 
