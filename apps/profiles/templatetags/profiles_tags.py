a844242165721d8704bff18f70f7b93fda9f7ed2
Sylvain Chevalier <sylvain@pculture.org>
2016-05-26 09:47:08 +0200

    Removed unused function

--- apps/profiles/templatetags/profiles_tags.py
+++ apps/profiles/templatetags/profiles_tags.py
@@ -72,19 +72,6 @@ def _user_needs_languages(context):
     else:
         return not bool(get_user_languages_from_cookie(context['request']))
 
-@register.inclusion_tag('profiles/_user_videos_activity.html', takes_context=True)
-def user_videos_activity(context, user=None):
-    user = user or context['user']
-
-    if user.is_authenticated():
-        context['users_actions'] = Action.objects.select_related('video', 'language', 'language__video', 'user') \
-            .filter(video__customuser=user) \
-            .exclude(user=user) \
-            .exclude(user=User.get_amara_anonymous())[:ACTIONS_ON_PAGE]
-    else:
-        context['users_actions'] = Action.objects.none()
-    return context
-
 @register.inclusion_tag('profiles/_user_avatar.html', takes_context=True)
 def user_avatar(context, user_obj):
     return {
