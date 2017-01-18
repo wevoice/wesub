8f1e3bb34656596383f115ee76756c9214485d1d
Ben Dean-Kawamura <ben@pculture.org>
2017-01-18 16:01:04 -0500

    Updated update-integration.py
    This should allow us to deploy topic branches more easily.

--- bin/update-integration.py
+++ bin/update-integration.py
@@ -17,6 +17,36 @@ def get_repo_names():
     return [f for f in os.listdir(optional_dir())
             if not f.startswith(".")]
 
+def get_topic_branch_name(repo_name):
+    """Get the name of a topic branch
+
+    This function checks if there's a branch with the same name on the
+    other repository as the one on the unisubs repository.  If so, we assume
+    that we're in topic branch mode and return "origin/[branch]".  If not,
+    then we return None
+    """
+    os.chdir(root_dir)
+    branch = get_branch_name()
+    if branch is None:
+        return None
+    else:
+        branch = "origin/" + branch
+    os.chdir(repo_dir(repo_name))
+    if branch_exists(branch):
+        return branch
+    else:
+        return None
+
+def get_branch_name():
+    cmd = ["git", "rev-parse", "--abbrev-ref", "HEAD"]
+    branch = subprocess.check_output(cmd).strip()
+    return branch if branch != "HEAD" else None
+
+def branch_exists(branch_name):
+    cmd = ["git", "branch", "-r", "--list", branch_name]
+    output = subprocess.check_output(cmd).strip()
+    return output.strip() == branch_name
+
 def get_repo_commit(repo_name):
     path = os.path.join(optional_dir(), repo_name)
     return open(path).read().strip()
@@ -34,16 +64,22 @@ def run_git_clone(repo_name):
     print "{0}: reset to {1}".format(repo_name, commit_id)
     run_command("git", "reset", "--hard", commit_id)
 
-def run_git_reset(repo_name, skip_fetch):
+def run_git_fetch(repo_name, skip_fetch):
     os.chdir(repo_dir(repo_name))
     if not skip_fetch:
         print "{0}: fetching".format(repo_name)
         run_command("git", "fetch", "origin")
     else:
         print "{0}: skipping fetch".format(repo_name)
-    commit_id = get_repo_commit(repo_name)
-    print "{0} reset to {1}".format(repo_name, commit_id)
-    run_command("git", "reset", "--hard", commit_id)
+
+def reset_to_commit(repo_name, topic_branch):
+    ref = None
+    if topic_branch:
+        ref = get_topic_branch_name(repo_name)
+    if ref is None:
+        ref = get_repo_commit(repo_name)
+    print "{0} reset to {1}".format(repo_name, ref)
+    run_command("git", "reset", "--hard", ref)
 
 def make_option_parser():
     parser = optparse.OptionParser()
@@ -51,6 +87,11 @@ def make_option_parser():
                       action='store_true', help="don't run git fetch")
     parser.add_option("--clone-missing", dest="clone_missing",
                       action='store_true', help="clone missing repositories")
+    parser.add_option("--topic-branch", dest="topic_branch",
+                      action='store_true',
+                      help="Use topic branch logic (use the HEAD of the "
+                      "branch with the same name as unisubs branch if it "
+                      "exists)")
     return parser
 
 def main(argv):
@@ -58,12 +99,13 @@ def main(argv):
     (options, args) = parser.parse_args(argv)
     for repo_name in get_repo_names():
         if os.path.exists(repo_dir(repo_name)):
-            run_git_reset(repo_name, options.skip_fetch)
+            run_git_fetch(repo_name, options.skip_fetch)
         elif options.clone_missing:
             run_git_clone(repo_name)
         else:
             print ("{0}: directory doesn't exist use --clone-missing "
                    "to create it".format(repo_name))
+        reset_to_commit(repo_name, options.topic_branch)
 
 if __name__ == '__main__':
     main(sys.argv)
