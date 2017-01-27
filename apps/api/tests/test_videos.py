8d007d813917309de2d39902ac09ada47876fbe4
Sylvain Chevalier <sylvain@pculture.org>
2017-01-27 23:51:39 +0100

    improved test

--- apps/api/tests/test_videos.py
+++ apps/api/tests/test_videos.py
@@ -31,6 +31,7 @@ from subtitles import pipeline
 from utils.factories import *
 from utils import test_utils
 from utils.test_utils.api import *
+from videos.models import Video
 import teams.signals
 
 class VideoSerializerTest(TestCase):
@@ -760,5 +761,7 @@ class VideoViewTestCase(TestCase):
         })
 
     def test_delete_video(self):
+        video_pk = self.video.pk
         response = self.client.delete(self.url)
         assert_equal(response.status_code, status.HTTP_204_NO_CONTENT)
+        assert_false(Video.objects.filter(pk=video_pk).exists())
