91d165d6b5f391396b86a9277c8bd5508f2abd5e
William Burton <william.burton@welocalize.com>
2017-02-08 15:23:22 -0500

    testing new fork

--- dev_settings.py
+++ dev_settings.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see
 # http://www.gnu.org/licenses/agpl-3.0.html.
+# First Change
 
 from datetime import timedelta
 from settings import *
